80726e3062510e937fba95de30d68f216d5960cf
Pavel Kirilin <kirilin.pav@gmail.com>
2019-11-10 15:38:03 +0300

    [back] Created project for unit tests

---
+++ tests/FoodDiary.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace FoodDiary.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
