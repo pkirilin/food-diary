4a207c59be72b8a48a148a56d965337af3103328
Pavel Kirilin <kirilin.pav@gmail.com>
2019-12-04 23:48:44 +0300

    [back] Service for products API implementation

---
+++ src/FoodDiary.Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FoodDiary.Domain.Entities;
+
+namespace FoodDiary.Domain.Repositories
+{
+    public interface IProductRepository
+    {
+        IQueryable<Product> GetQuery();
+
+        IQueryable<Product> GetQueryWithoutTracking();
+
+        Task<List<Product>> GetListFromQueryAsync(IQueryable<Product> query, CancellationToken cancellationToken);
+
+        Task<int> CountByQueryAsync(IQueryable<Product> productsQuery, CancellationToken cancellationToken);
+
+        Task<Product> GetByIdAsync(int id, CancellationToken cancellationToken);
+
+        Task<IEnumerable<Product>> GetByIdsAsync(IEnumerable<int> ids, CancellationToken cancellationToken);
+
+        Task<bool> IsDuplicateAsync(string productName, CancellationToken cancellationToken);
+
+        Product Create(Product product);
+
+        Product Update(Product product);
+
+        Product Delete(Product product);
+
+        void DeleteRange(IEnumerable<Product> products);
+
+        Task SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ src/FoodDiary.Infrastructure/Services/ProductService.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FoodDiary.Domain.Dtos;
+using FoodDiary.Domain.Entities;
+using FoodDiary.Domain.Repositories;
+using FoodDiary.Domain.Services;
+
+namespace FoodDiary.Infrastructure.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<int> CountAllProductsAsync(CancellationToken cancellationToken)
+        {
+            var q = _productRepository.GetQueryWithoutTracking();
+            var count = await _productRepository.CountByQueryAsync(q, cancellationToken);
+            return count;
+        }
+
+        public async Task<IEnumerable<Product>> SearchProductsAsync(ProductsSearchRequestDto searchRequest, CancellationToken cancellationToken)
+        {
+            var searchQuery = _productRepository.GetQueryWithoutTracking();
+
+            if (!String.IsNullOrWhiteSpace(searchRequest.ProductSearchName))
+            {
+                searchQuery = searchQuery.Where(p =>
+                    p.Name.ToLower()
+                        .StartsWith(searchRequest.ProductSearchName.ToLower()));
+            }
+
+            if (searchRequest.CategoryId.HasValue)
+            {
+                searchQuery = searchQuery.Where(p => p.CategoryId == searchRequest.CategoryId);
+            }
+
+            searchQuery = searchQuery.Skip((searchRequest.PageIndex - 1) * searchRequest.PageSize)
+                .Take(searchRequest.PageSize);
+            return await _productRepository.GetListFromQueryAsync(searchQuery, cancellationToken);
+        }
+
+        public async Task<Product> GetProductByIdAsync(int id, CancellationToken cancellationToken)
+        {
+            return await _productRepository.GetByIdAsync(id, cancellationToken);
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsByIdsAsync(IEnumerable<int> ids, CancellationToken cancellationToken)
+        {
+            return await _productRepository.GetByIdsAsync(ids, cancellationToken);
+        }
+
+        public async Task<ValidationResultDto> ValidateProductAsync(ProductCreateEditDto productData, CancellationToken cancellationToken)
+        {
+            if (!await _productRepository.IsDuplicateAsync(productData.Name, cancellationToken))
+            {
+                return new ValidationResultDto(false, $"Product with the name '{productData.Name}' already exists");
+            }
+
+            return new ValidationResultDto(true);
+        }
+
+        public bool IsEditedProductValid(ProductCreateEditDto editedProductData, Product originalProduct, ValidationResultDto editedProductValidationResult)
+        {
+            bool productHasChanges = editedProductData.Name != originalProduct.Name;
+            return !productHasChanges
+                || (productHasChanges && editedProductValidationResult.IsValid);
+        }
+
+        public ValidationResultDto AllProductsFetched(IEnumerable<Product> fetchedProducts, IEnumerable<int> requestedIds)
+        {
+            var fetchedProductsIds = fetchedProducts.Select(p => p.Id);
+            if (fetchedProductsIds.Except(requestedIds).Any())
+            {
+                return new ValidationResultDto(false, "Products cannot be deleted: fetched product ids mismatch requested ids");
+            }
+
+            return new ValidationResultDto(true);
+        }
+
+        public async Task<Product> CreateProductAsync(Product product, CancellationToken cancellationToken)
+        {
+            var createdProduct = _productRepository.Create(product);
+            await _productRepository.SaveChangesAsync(cancellationToken);
+            return createdProduct;
+        }
+
+        public async Task<Product> EditProductAsync(Product product, CancellationToken cancellationToken)
+        {
+            var updatedProduct = _productRepository.Update(product);
+            await _productRepository.SaveChangesAsync(cancellationToken);
+            return updatedProduct;
+        }
+
+        public async Task<Product> DeleteProductAsync(Product product, CancellationToken cancellationToken)
+        {
+            var deletedProduct = _productRepository.Delete(product);
+            await _productRepository.SaveChangesAsync(cancellationToken);
+            return deletedProduct;
+        }
+
+        public async Task DeleteProductsRangeAsync(IEnumerable<Product> products, CancellationToken cancellationToken)
+        {
+            _productRepository.DeleteRange(products);
+            await _productRepository.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
