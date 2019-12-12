7dbbad4ea0a1be0b3b01ae3aa9cf0ba6a713df5a
Pavel Kirilin <kirilin.pav@gmail.com>
2019-12-12 22:50:18 +0300

    [back] Added lowercasing of first letter in model state keys to beautify the result JSON

--- src/FoodDiary.Domain/Dtos/ValidationResultDto.cs
+++ src/FoodDiary.Domain/Dtos/ValidationResultDto.cs
@@ -4,30 +4,44 @@ namespace FoodDiary.Domain.Dtos
 {
     public struct ValidationResultDto
     {
+        private readonly string _errorKey;
+
         public bool IsValid { get; set; }
 
-        public string ErrorKey { get; set; }
+        public string ErrorKey
+        {
+            get
+            {
+                if (String.IsNullOrWhiteSpace(_errorKey))
+                    return String.Empty;
+
+                // Converting first letter of key to lower case as this key will be sent to the client in JSON
+                if (_errorKey.Length == 1)
+                    return _errorKey.ToLower();
+                return String.Concat(_errorKey.Substring(0, 1).ToLower(), _errorKey.Substring(1));
+            }
+        }
 
         public string ErrorMessage { get; set; }
 
         public ValidationResultDto(bool isValid)
         {
             IsValid = isValid;
-            ErrorKey = String.Empty;
+            _errorKey = String.Empty;
             ErrorMessage = String.Empty;
         }
 
         public ValidationResultDto(bool isValid, string errorMessage)
         {
             IsValid = isValid;
-            ErrorKey = String.Empty;
+            _errorKey = String.Empty;
             ErrorMessage = errorMessage;
         }
 
         public ValidationResultDto(bool isValid, string modelStateKey, string errorMessage)
         {
             IsValid = isValid;
-            ErrorKey = modelStateKey;
+            _errorKey = modelStateKey;
             ErrorMessage = errorMessage;
         }
     }
