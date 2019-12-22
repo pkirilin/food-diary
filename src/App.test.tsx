14069fca6bb9bab4ccdf4c13f3d214534d8eb6d4
Pavel Kirilin <kirilin.pav@gmail.com>
2019-12-22 18:38:30 +0300

    [front] Initial setup
    - Changed some auto generated code
    - Added node-sass
    - Added scss instead of css
    - Changed favicon
    - Added prettier settings for css & scss
    - Added first test content to App component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  it('should render without crashing', () => {
+    shallow(<App></App>);
+  });
 });
