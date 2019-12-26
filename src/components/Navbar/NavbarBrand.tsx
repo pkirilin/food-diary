1973231dab7afc2a8d8b8276eff971786158551e
Pavel Kirilin <kirilin.pav@gmail.com>
2019-12-26 22:52:24 +0300

    [front] Added navbar logo

--- src/components/Navbar/NavbarBrand.tsx
+++ src/components/Navbar/NavbarBrand.tsx
@@ -5,6 +5,9 @@ import './NavbarBrand.scss';
 const NavbarBrand: React.FC = () => {
   return (
     <div className="navbar__brand">
+      <Link to="/">
+        <span className="navbar__brand_logo"></span>
+      </Link>
       <Link to="/">Food diary</Link>
     </div>
   );
