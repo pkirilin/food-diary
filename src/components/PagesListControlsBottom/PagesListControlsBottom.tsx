88be841221a634c5dfccdfef0550b9b64701a273
Pavel Kirilin <kirilin.pav@gmail.com>
2020-01-16 21:41:29 +0300

    [front] Added pages list controls bottom UI

--- src/components/PagesListControlsBottom/PagesListControlsBottom.tsx
+++ src/components/PagesListControlsBottom/PagesListControlsBottom.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import './PagesListControlsBottom.scss';
 import { IconSortDescending } from '../Icons';
+import { Label, Dropdown, DropdownItem, FormGroup } from '../Controls';
 
 const PagesListControlsBottom: React.FC = () => {
   return (
-    <div className="fd-pages-list-controls-bottom">
+    <div className="pages-list-controls-bottom">
       <IconSortDescending></IconSortDescending>
-      <div className="fd-pages-list-controls-bottom__show-count">Show: All</div>
+      <FormGroup inline>
+        <Label>Show</Label>
+        <div className="pages-list-controls-bottom__show-count-wrapper">
+          <Dropdown initialSelectedValue="30" toggleDirection="top">
+            <DropdownItem>7</DropdownItem>
+            <DropdownItem>30</DropdownItem>
+            <DropdownItem>60</DropdownItem>
+            <DropdownItem>90</DropdownItem>
+            <DropdownItem>120</DropdownItem>
+            <DropdownItem>365</DropdownItem>
+            <DropdownItem>All</DropdownItem>
+          </Dropdown>
+        </div>
+      </FormGroup>
     </div>
   );
 };
