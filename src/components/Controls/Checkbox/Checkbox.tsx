9a5ac55698ef5349d37047e4ba64d5a4df814cbc
Pavel Kirilin <kirilin.pav@gmail.com>
2020-03-07 21:13:33 +0300

    [front] Added disabled prop to checkbox

--- src/components/Controls/Checkbox/Checkbox.tsx
+++ src/components/Controls/Checkbox/Checkbox.tsx
@@ -5,18 +5,30 @@ interface CheckboxProps {
   checked: boolean;
   onCheck: React.ChangeEventHandler<HTMLInputElement>;
   label?: string;
+  disabled?: boolean;
 }
 
-const Checkbox: React.FC<CheckboxProps> = ({ checked, onCheck, label = '' }: CheckboxProps) => {
+const Checkbox: React.FC<CheckboxProps> = ({ checked, onCheck, label = '', disabled = false }: CheckboxProps) => {
   const containerClassNames = ['checkbox-container'];
+
   if (label !== '') {
     containerClassNames.push('checkbox-container_labeled');
   }
 
+  if (disabled) {
+    containerClassNames.push('checkbox-container_disabled');
+  }
+
   return (
     <label className={containerClassNames.join(' ')}>
+      <input
+        type="checkbox"
+        className="checkbox-container__input"
+        onChange={onCheck}
+        checked={checked}
+        disabled={disabled}
+      />
       <div className="checkbox-container__label">{label}</div>
-      <input type="checkbox" className="checkbox-container__input" onChange={onCheck} checked={checked} />
       <span className="checkbox-container__checkmark"></span>
     </label>
   );
