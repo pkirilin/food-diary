1257e8a7e7c8ebbf5801cc30750fa87bc14ef58d
Pavel Kirilin <kirilin.pav@gmail.com>
2020-05-03 20:24:50 +0300

    Simplified PageContentHeader

--- src/components/PageContentHeader/PageContentHeader.tsx
+++ src/components/PageContentHeader/PageContentHeader.tsx
@@ -4,7 +4,6 @@ import { PageContentHeaderStateToPropsMapResult } from './PageContentHeaderConne
 import Icon from '../Icon';
 import { NavLink } from 'react-router-dom';
 import MealsControlPanelConnected from '../MealsControlPanel/MealsControlPanelConnected';
-import Loader from '../Loader';
 import { getFormattedDate } from '../../utils/date-utils';
 import { useIdFromRoute } from '../../hooks';
 
@@ -30,46 +29,45 @@ const PageContentHeader: React.FC<PageContentHeaderProps> = ({
   const previousPageId = visiblePagesIds[currentPageIndex - 1];
   const nextPageId = visiblePagesIds[currentPageIndex + 1];
 
-  const previousPageIcon = (
-    <Icon
-      type="next-arrow"
-      svgStyle={{
-        width: '25px',
-        height: '25px',
-        transform: 'rotate(180deg)',
-      }}
-      disabled={!isPreviousPageIconActive}
-    ></Icon>
-  );
+  const previousPageLinkClassNames: string[] = [];
+  const nextPageLinkClassNames: string[] = [];
 
-  const nextPageIcon = (
-    <Icon
-      type="next-arrow"
-      svgStyle={{
-        width: '25px',
-        height: '25px',
-      }}
-      disabled={!isNextPageIconActive}
-    ></Icon>
-  );
+  if (!isPreviousPageIconActive) {
+    previousPageLinkClassNames.push('page-content-header__navigation_disabled');
+  }
+
+  if (!isNextPageIconActive) {
+    nextPageLinkClassNames.push('page-content-header__navigation_disabled');
+  }
 
   return (
     <div className="page-content-header">
       <div className="page-content-header__navigation-wrapper">
         <div className="page-content-header__navigation">
-          {isPreviousPageIconActive ? (
-            <NavLink to={`/pages/${previousPageId}`}>{previousPageIcon}</NavLink>
-          ) : (
-            previousPageIcon
-          )}
-          {isPageContentLoading ? (
-            <Loader size="small" label="Loading date"></Loader>
-          ) : (
-            <div className="page-content-header__navigation__date">
-              {currentSelectedPage && getFormattedDate(currentSelectedPage.date)}
-            </div>
-          )}
-          {isNextPageIconActive ? <NavLink to={`/pages/${nextPageId}`}>{nextPageIcon}</NavLink> : nextPageIcon}
+          <NavLink to={`/pages/${previousPageId}`} className={previousPageLinkClassNames.join(' ')}>
+            <Icon
+              type="next-arrow"
+              svgStyle={{
+                width: '25px',
+                height: '25px',
+                transform: 'rotate(180deg)',
+              }}
+              disabled={!isPreviousPageIconActive}
+            ></Icon>
+          </NavLink>
+          <div className="page-content-header__navigation__date">
+            {currentSelectedPage && getFormattedDate(currentSelectedPage.date)}
+          </div>
+          <NavLink to={`/pages/${nextPageId}`} className={nextPageLinkClassNames.join(' ')}>
+            <Icon
+              type="next-arrow"
+              svgStyle={{
+                width: '25px',
+                height: '25px',
+              }}
+              disabled={!isNextPageIconActive}
+            ></Icon>
+          </NavLink>
         </div>
       </div>
       <MealsControlPanelConnected></MealsControlPanelConnected>
