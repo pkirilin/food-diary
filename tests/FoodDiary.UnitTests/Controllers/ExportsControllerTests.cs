9d01037efce1284817d3e4dd461fd1f1037e3fbe
Pavel Kirilin <kirilin.pav@gmail.com>
2020-05-07 21:26:01 +0300

    Added unit tests for PDF export pages controller and service

---
+++ tests/FoodDiary.UnitTests/Controllers/ExportsControllerTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using AutoFixture;
+using FluentAssertions;
+using FoodDiary.API.Controllers.v1;
+using FoodDiary.Domain.Dtos;
+using FoodDiary.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace FoodDiary.UnitTests.Controllers
+{
+    public class ExportsControllerTests
+    {
+        private readonly Mock<IExportService> _exportServiceMock;
+
+        private readonly IFixture _fixture;
+
+        public ExportsControllerTests()
+        {
+            _exportServiceMock = new Mock<IExportService>();
+            _fixture = SetupFixture();
+        }
+
+        private IFixture SetupFixture()
+        {
+            return new Fixture();
+        }
+
+        public ExportsController ExportsController
+        {
+            get
+            {
+                return new ExportsController(_exportServiceMock.Object);
+            }
+        }
+
+        [Theory]
+        [InlineData("2020-05-06", "2020-05-07")]
+        [InlineData("2020-05-06", "2020-05-06")]
+        public async void ExportPagesPdf_ReturnsExportFileContent_WhenStartDateLessThanOrEqualEndDate(string startDateStr, string endDateStr)
+        {
+            var request = _fixture.Build<PagesExportRequestDto>()
+                .With(r => r.StartDate, DateTime.Parse(startDateStr))
+                .With(r => r.EndDate, DateTime.Parse(endDateStr))
+                .Create();
+            _exportServiceMock.Setup(s => s.GetExportPagesPdfContentsAsync(request.StartDate, request.EndDate, CancellationToken.None))
+                .ReturnsAsync(_fixture.CreateMany<byte>().ToArray());
+
+            var result = await ExportsController.ExportPagesPdf(request, CancellationToken.None);
+
+            _exportServiceMock.Verify(s => s.GetExportPagesPdfContentsAsync(request.StartDate, request.EndDate, CancellationToken.None), Times.Once);
+            result.Should().BeOfType<FileContentResult>();
+        }
+
+        [Theory]
+        [InlineData("2020-05-07", "2020-05-06")]
+        public async void ExportPagesPdf_ReturnsBadRequest_WhenStartDateGreaterThanEndDate(string startDateStr, string endDateStr)
+        {
+            var request = _fixture.Build<PagesExportRequestDto>()
+                .With(r => r.StartDate, DateTime.Parse(startDateStr))
+                .With(r => r.EndDate, DateTime.Parse(endDateStr))
+                .Create();
+
+            var result = await ExportsController.ExportPagesPdf(request, CancellationToken.None);
+
+            _exportServiceMock.Verify(s => s.GetExportPagesPdfContentsAsync(request.StartDate, request.EndDate, CancellationToken.None), Times.Never);
+            result.Should().BeOfType<BadRequestObjectResult>();
+        }
+    }
+}
---
+++ tests/FoodDiary.UnitTests/Services/ExportServiceTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using AutoFixture;
+using FluentAssertions;
+using FoodDiary.Domain.Entities;
+using FoodDiary.Domain.Repositories;
+using FoodDiary.Domain.Services;
+using FoodDiary.Infrastructure.Services;
+using FoodDiary.Pdf;
+using FoodDiary.UnitTests.Customizations;
+using Moq;
+using Xunit;
+
+namespace FoodDiary.UnitTests.Services
+{
+    public class ExportServiceTests
+    {
+        private readonly Mock<IPageRepository> _pageRepositoryMock;
+        private readonly Mock<IPagesPdfGenerator> _pagesPdfGeneratorMock;
+
+        private readonly IFixture _fixture;
+
+        public ExportServiceTests()
+        {
+            _pageRepositoryMock = new Mock<IPageRepository>();
+            _pagesPdfGeneratorMock = new Mock<IPagesPdfGenerator>();
+            _fixture = SetupFixture();
+        }
+
+        private IFixture SetupFixture()
+        {
+            var _fixture = new Fixture();
+            _fixture.Customize(new FixtureWithCircularReferencesCustomization());
+            return _fixture;
+        }
+
+        public IExportService ExportService
+            => new ExportService(_pageRepositoryMock.Object, _pagesPdfGeneratorMock.Object);
+
+        [Fact]
+        public async void GetExportPagesPdfContents_ReturnsPagesPdfFileBytes()
+        {
+            var startDate = _fixture.Create<DateTime>();
+            var endDate = _fixture.Create<DateTime>();
+            var pagesForExport = _fixture.CreateMany<Page>();
+            var pdfFileBytes = _fixture.CreateMany<byte>().ToArray();
+            _pageRepositoryMock.Setup(r => r.GetListFromQueryAsync(It.IsNotNull<IQueryable<Page>>(), CancellationToken.None))
+                .ReturnsAsync(pagesForExport);
+            _pagesPdfGeneratorMock.Setup(g => g.GeneratePdfForPages(pagesForExport))
+                .Returns(pdfFileBytes);
+
+            var result = await ExportService.GetExportPagesPdfContentsAsync(startDate, endDate, CancellationToken.None);
+
+            _pageRepositoryMock.Verify(r => r.GetQueryWithoutTracking(), Times.Once);
+            _pageRepositoryMock.Verify(r => r.LoadNotesWithProducts(It.IsNotNull<IQueryable<Page>>()), Times.Once);
+            _pageRepositoryMock.Verify(r => r.GetListFromQueryAsync(It.IsNotNull<IQueryable<Page>>(), CancellationToken.None), Times.Once);
+            _pagesPdfGeneratorMock.Verify(g => g.GeneratePdfForPages(pagesForExport), Times.Once);
+            result.Should().Contain(pdfFileBytes);
+        }
+    }
+}
