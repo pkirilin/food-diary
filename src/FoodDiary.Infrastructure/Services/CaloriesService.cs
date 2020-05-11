3196d51596fc5ab8072202c8dbb02473522110e3
Pavel Kirilin <kirilin.pav@gmail.com>
2020-05-11 11:50:30 +0300

    Fixed argument checks for calories calculator

--- src/FoodDiary.Infrastructure/Services/CaloriesService.cs
+++ src/FoodDiary.Infrastructure/Services/CaloriesService.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using FoodDiary.Domain.Services;
 
 namespace FoodDiary.Infrastructure.Services
@@ -11,9 +11,9 @@ namespace FoodDiary.Infrastructure.Services
 
         public double CalculateForQuantity(double caloriesCost, double quantity)
         {
-            if (caloriesCost < 1)
+            if (caloriesCost < 0)
                 throw new ArgumentException("Calories cost value must be a positive number", nameof(caloriesCost));
-            if (quantity < 1)
+            if (quantity < 0)
                 throw new ArgumentException("Quantity value must be a positive number", nameof(quantity));
 
             return caloriesCost * quantity / 100;
