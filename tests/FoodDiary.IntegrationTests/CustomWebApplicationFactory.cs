8129067256e2dcc6b91dcfabef18c85d581bbe31
Pavel Kirilin <kirilin.pav@gmail.com>
2020-07-31 21:17:05 +0300

    Created and set project for integration tests

---
+++ tests/FoodDiary.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+
+namespace FoodDiary.IntegrationTests
+{
+    public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override IWebHostBuilder CreateWebHostBuilder()
+        {
+            var webHostBuilder = WebHost.CreateDefaultBuilder()
+                .ConfigureAppConfiguration(builder =>
+                {
+                    builder.AddJsonFile("appsettings.json", false);
+                    builder.AddEnvironmentVariables();
+                })
+                .UseStartup<TStartup>();
+
+            return webHostBuilder;
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseContentRoot(".");
+            base.ConfigureWebHost(builder);
+        }
+    }
+}
---
+++ tests/FoodDiary.IntegrationTests/TestStartup.cs
@@ -0,0 +1,79 @@
+﻿using AutoMapper;
+using FoodDiary.API;
+using FoodDiary.API.Controllers.v1;
+using FoodDiary.API.Extensions;
+using FoodDiary.API.Middlewares;
+using FoodDiary.API.Options;
+using FoodDiary.Import.Extensions;
+using FoodDiary.Infrastructure;
+using FoodDiary.PdfGenerator.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace FoodDiary.IntegrationTests
+{
+    public class TestStartup
+    {
+        public TestStartup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<ImportOptions>(Configuration.GetSection("Import"));
+
+            // Injecting in-memory db context as a singleton to share stored data across multiple requests
+            services.AddDbContext<FoodDiaryContext>(options =>
+            {
+                var sp = new ServiceCollection()
+                    .AddEntityFrameworkInMemoryDatabase()
+                    .BuildServiceProvider();
+
+                options.UseInMemoryDatabase("InMemoryAppDb");
+                options.UseInternalServiceProvider(sp);
+            }, ServiceLifetime.Singleton);
+
+            services.AddRepositories();
+            services.AddAppServices();
+            services.AddUtils();
+            services.AddPagesPdfGenerator();
+            services.AddPagesJsonImportServices();
+
+            services.AddAutoMapper(typeof(AutoMapperProfile).Assembly);
+
+            services.AddControllers()
+                .AddApplicationPart(typeof(CategoriesController).Assembly);
+            services.AddFoodDiarySwagger();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            SeedDatabaseForIntegrationTests(app);
+
+            app.UseRouting();
+            app.UseMiddleware<ExceptionHandlerMiddleware>();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+        private void SeedDatabaseForIntegrationTests(IApplicationBuilder app)
+        {
+            var context = app.ApplicationServices.GetService<FoodDiaryContext>();
+            context.Database.EnsureCreated();
+        }
+    }
+}
