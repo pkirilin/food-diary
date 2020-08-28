08f42a7ef139ed270c17b5de4954b061ed8e108f
Pavel Kirilin <kirilin.pav@gmail.com>
2020-08-28 21:23:36 +0300

    Added request and handler for pages json import

---
+++ src/FoodDiary.Application/Imports/Requests/PagesJsonImportRequest.cs
@@ -0,0 +1,15 @@
+﻿using FoodDiary.Import.Models;
+using MediatR;
+
+namespace FoodDiary.Application.Imports.Requests
+{
+    public class PagesJsonImportRequest : IRequest<int>
+    {
+        public PagesJsonObject JsonObj { get; set; }
+
+        public PagesJsonImportRequest(PagesJsonObject jsonObj)
+        {
+            JsonObj = jsonObj;
+        }
+    }
+}
