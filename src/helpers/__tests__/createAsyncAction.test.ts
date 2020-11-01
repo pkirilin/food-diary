d46802e60084d20add3754164c64687426361dc1
Pavel Kirilin <kirilin.pav@gmail.com>
2020-11-01 18:50:12 +0300

    Added 'modifyUrl' and 'constructBody' parameters for apiOptions

--- src/helpers/__tests__/createAsyncAction.test.ts
+++ src/helpers/__tests__/createAsyncAction.test.ts
@@ -1,17 +1,324 @@
-import { ErrorAction, RequestAction, SuccessAction } from '../createAsyncAction';
+import configureStore from 'redux-mock-store';
+import thunk, { ThunkDispatch } from 'redux-thunk';
+import { ApiRequestBody, createAsyncAction, ErrorAction, RequestAction, SuccessAction } from '../createAsyncAction';
 
-enum TestActionTypes {
-  Request = 'TEST_REQUEST',
-  Success = 'TEST_SUCCESS',
-  Error = 'TEST_ERROR',
+const mockStore = configureStore<TestRecordsState, GetTestRecordsDispatch & CreateTestRecordDispatch>([thunk]);
+const store = mockStore();
+const fetchMock = jest.fn() as jest.Mock<Promise<Response>>;
+
+global.fetch = fetchMock;
+
+type TestRecord = {
+  id: number;
+  name: string;
+  age: number;
+};
+
+type TestRecordsState = {
+  records: TestRecord[];
+};
+
+type GetTestRecordsRequest = {
+  filterByName: string;
+  ageLimit: number;
+};
+
+enum GetTestRecordsActionTypes {
+  Request = 'GET_TEST_RECORDS_REQUEST',
+  Success = 'GET_TEST_RECORDS_SUCCESS',
+  Error = 'GET_TEST_RECORDS_ERROR',
 }
 
-type TestRequestAction = RequestAction<TestActionTypes.Request>;
-type TestSuccessAction = SuccessAction<TestActionTypes.Success>;
-type TestErrorAction = ErrorAction<TestActionTypes.Error>;
+enum CreateTestRecordActionTypes {
+  Request = 'CREATE_TEST_RECORD_REQUEST',
+  Success = 'CREATE_TEST_RECORD_SUCCESS',
+  Error = 'CREATE_TEST_RECORD_ERROR',
+}
+
+type GetTestRecordsRequestAction = RequestAction<GetTestRecordsActionTypes.Request, GetTestRecordsRequest>;
+type GetTestRecordsSuccessAction = SuccessAction<GetTestRecordsActionTypes.Success, TestRecord[]>;
+type GetTestRecordsErrorAction = ErrorAction<GetTestRecordsActionTypes.Error>;
+
+type CreateTestRecordRequestAction = RequestAction<CreateTestRecordActionTypes.Request, TestRecord>;
+type CreateTestRecordSuccessAction = SuccessAction<CreateTestRecordActionTypes.Success>;
+type CreateTestRecordErrorAction = ErrorAction<CreateTestRecordActionTypes.Error>;
+
+type GetTestRecordsActions = GetTestRecordsRequestAction | GetTestRecordsSuccessAction | GetTestRecordsErrorAction;
+type CreateTestRecordActions =
+  | CreateTestRecordRequestAction
+  | CreateTestRecordSuccessAction
+  | CreateTestRecordErrorAction;
+
+type GetTestRecordsDispatch = ThunkDispatch<
+  TestRecord[],
+  GetTestRecordsRequest,
+  GetTestRecordsSuccessAction | GetTestRecordsErrorAction
+>;
+type CreateTestRecordDispatch = ThunkDispatch<
+  {},
+  TestRecord,
+  CreateTestRecordSuccessAction | CreateTestRecordErrorAction
+>;
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+function generateGetTestRecordsAsyncAction() {
+  return createAsyncAction<
+    GetTestRecordsRequestAction,
+    GetTestRecordsSuccessAction,
+    GetTestRecordsErrorAction,
+    GetTestRecordsActionTypes.Request,
+    GetTestRecordsActionTypes.Success,
+    GetTestRecordsActionTypes.Error,
+    TestRecord[],
+    GetTestRecordsRequest
+  >({
+    makeRequest: () => {
+      return (payload?: GetTestRecordsRequest): GetTestRecordsRequestAction => {
+        if (!payload) {
+          throw new Error('Failed to get test records: request payload is empty');
+        }
+
+        return {
+          type: GetTestRecordsActionTypes.Request,
+          requestMessage: 'Loading test records',
+          payload,
+        };
+      };
+    },
+    makeSuccess: () => {
+      return (data?: TestRecord[]): GetTestRecordsSuccessAction => ({
+        type: GetTestRecordsActionTypes.Success,
+        data: data ?? [],
+      });
+    },
+    makeError: () => {
+      return (errorMessage?: string): GetTestRecordsErrorAction => ({
+        type: GetTestRecordsActionTypes.Error,
+        errorMessage: errorMessage ?? '',
+      });
+    },
+    apiOptions: {
+      baseUrl: 'test url',
+      onSuccess: async (response): Promise<TestRecord[]> => {
+        const data = (await response.json()) as TestRecord[];
+        return data;
+      },
+      onError: (): string => {
+        return 'test error message';
+      },
+    },
+  });
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+function generateCreateTestRecordAsyncAction() {
+  return createAsyncAction<
+    CreateTestRecordRequestAction,
+    CreateTestRecordSuccessAction,
+    CreateTestRecordErrorAction,
+    CreateTestRecordActionTypes.Request,
+    CreateTestRecordActionTypes.Success,
+    CreateTestRecordActionTypes.Error,
+    {},
+    TestRecord
+  >({
+    makeRequest: () => {
+      return (payload?: TestRecord): CreateTestRecordRequestAction => {
+        if (!payload) {
+          throw new Error('Failed to create test record: payload is empty');
+        }
+
+        return {
+          type: CreateTestRecordActionTypes.Request,
+          requestMessage: 'Creating test record',
+          payload,
+        };
+      };
+    },
+    makeSuccess: () => {
+      return (): CreateTestRecordSuccessAction => ({
+        type: CreateTestRecordActionTypes.Success,
+        data: {},
+      });
+    },
+    makeError: () => {
+      return (errorMessage?: string): CreateTestRecordErrorAction => ({
+        type: CreateTestRecordActionTypes.Error,
+        errorMessage: errorMessage ?? '',
+      });
+    },
+    apiOptions: {
+      baseUrl: 'create test record base url',
+      method: 'POST',
+      modifyUrl: (baseUrl, payload): string => `${baseUrl} - (payload id = '${payload.id}')`,
+      constructBody: (payload: TestRecord): ApiRequestBody => JSON.stringify(payload),
+    },
+  });
+}
 
 describe('createAsyncAction', () => {
-  test('', () => {
-    //
+  afterEach(() => {
+    jest.clearAllMocks();
+    store.clearActions();
+  });
+
+  test('should create async action which dispatches request and success actions, calls onSuccess callback and receives data if server response is ok', async () => {
+    // Arrange
+    const getTestRecords = generateGetTestRecordsAsyncAction();
+    const requestPayload: GetTestRecordsRequest = {
+      filterByName: 'John',
+      ageLimit: 40,
+    };
+    const receivedRecords: TestRecord[] = [
+      {
+        id: 1,
+        name: 'John',
+        age: 34,
+      },
+      {
+        id: 2,
+        name: 'Mike',
+        age: 26,
+      },
+    ];
+    const expectedActions: GetTestRecordsActions[] = [
+      {
+        type: GetTestRecordsActionTypes.Request,
+        requestMessage: 'Loading test records',
+        payload: requestPayload,
+      },
+      {
+        type: GetTestRecordsActionTypes.Success,
+        data: receivedRecords,
+      },
+    ];
+
+    fetchMock.mockResolvedValue({
+      ...new Response(),
+      ok: true,
+      json: jest.fn().mockResolvedValue(receivedRecords),
+    });
+
+    // Act
+    await store.dispatch(getTestRecords(requestPayload));
+
+    // Assert
+    expect(store.getActions()).toEqual(expectedActions);
+    expect(fetchMock).toHaveBeenCalledWith('test url', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  });
+
+  test('should create async action which dispatches request and error actions and calls onError callback if server response is not ok', async () => {
+    // Arrange
+    const getTestRecords = generateGetTestRecordsAsyncAction();
+    const requestPayload: GetTestRecordsRequest = {
+      filterByName: 'John',
+      ageLimit: 40,
+    };
+    const expectedActions: GetTestRecordsActions[] = [
+      {
+        type: GetTestRecordsActionTypes.Request,
+        requestMessage: 'Loading test records',
+        payload: requestPayload,
+      },
+      {
+        type: GetTestRecordsActionTypes.Error,
+        errorMessage: 'test error message',
+      },
+    ];
+
+    fetchMock.mockResolvedValue({
+      ...new Response(),
+      ok: false,
+    });
+
+    // Act
+    await store.dispatch(getTestRecords(requestPayload));
+
+    // Assert
+    expect(store.getActions()).toEqual(expectedActions);
+    expect(fetchMock).toHaveBeenCalledWith('test url', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  });
+
+  test('should create async action which dispatches request and error actions and calls onError callback if server response is rejected', async () => {
+    // Arrange
+    const getTestRecords = generateGetTestRecordsAsyncAction();
+    const requestPayload: GetTestRecordsRequest = {
+      filterByName: 'John',
+      ageLimit: 40,
+    };
+    const expectedActions: GetTestRecordsActions[] = [
+      {
+        type: GetTestRecordsActionTypes.Request,
+        requestMessage: 'Loading test records',
+        payload: requestPayload,
+      },
+      {
+        type: GetTestRecordsActionTypes.Error,
+        errorMessage: 'Failed to fetch data: server is not available',
+      },
+    ];
+
+    fetchMock.mockRejectedValue(new Response());
+
+    // Act
+    await store.dispatch(getTestRecords(requestPayload));
+
+    // Assert
+    expect(store.getActions()).toEqual(expectedActions);
+    expect(fetchMock).toHaveBeenCalledWith('test url', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  });
+
+  test('should create async action which modifies base url by action parameter, POSTs test record and dispatches request and success actions if server response is ok', async () => {
+    // Arrange
+    const createTestRecord = generateCreateTestRecordAsyncAction();
+    const record: TestRecord = {
+      id: 1,
+      name: 'Mike',
+      age: 26,
+    };
+    const expectedActions: CreateTestRecordActions[] = [
+      {
+        type: CreateTestRecordActionTypes.Request,
+        requestMessage: 'Creating test record',
+        payload: record,
+      },
+      {
+        type: CreateTestRecordActionTypes.Success,
+        data: {},
+      },
+    ];
+
+    fetchMock.mockResolvedValue({
+      ...new Response(),
+      ok: true,
+    });
+
+    // Act
+    await store.dispatch(createTestRecord(record));
+
+    // Assert
+    expect(store.getActions()).toEqual(expectedActions);
+    expect(fetchMock).toHaveBeenCalledWith("create test record base url - (payload id = '1')", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(record),
+    });
   });
 });
--- src/helpers/createAsyncAction.ts
+++ src/helpers/createAsyncAction.ts
@@ -18,27 +18,33 @@ export type AsyncActionApiCall = {
   setUrl(url: string): AsyncActionApiCall;
 };
 
-export type ActionCreatorComposer<A> = () => A;
-export type ActionCreatorParameterizedComposer<A, D> = (data?: D) => A;
+export type ActionCreatorComposer<A, T> = (data?: T) => A;
 
 export type ApiMethod = 'GET' | 'POST' | 'PUT' | 'DELETE';
-
+export type ApiRequestBody = string | FormData | null;
 export type ApiResponseHandler<A extends Action, R> = (response: Response, dispatch: Dispatch<A>) => R | Promise<R>;
+export type ApiRequestUrlModifier<P> = (baseUrl: string, payload: P) => string;
+export type ApiRequestBodyConstructor<P> = (payload: P) => ApiRequestBody;
 
-export interface ApiOptions<SA extends Action, EA extends Action, D> {
-  url: string;
+export interface ApiOptions<SA extends Action, EA extends Action, D, P> {
+  baseUrl: string;
   method?: ApiMethod;
   contentType?: string;
   onSuccess?: ApiResponseHandler<SA, D>;
   onError?: ApiResponseHandler<EA, string>;
+  modifyUrl?: ApiRequestUrlModifier<P>;
+  constructBody?: ApiRequestBodyConstructor<P>;
 }
 
-export interface AsyncActionBuilderOptions<RA extends Action, SA extends Action, EA extends Action, D> {
-  makeRequest(): ActionCreatorComposer<RA>;
-  makeSuccess(): ActionCreatorParameterizedComposer<SA, D>;
-  makeError(): ActionCreatorParameterizedComposer<EA, string>;
-  apiOptions: ApiOptions<SA, EA, D>;
+export interface AsyncActionBuilderOptions<RA extends Action, SA extends Action, EA extends Action, D, P> {
+  makeRequest(): ActionCreatorComposer<RA, P>;
+  makeSuccess(): ActionCreatorComposer<SA, D>;
+  makeError(): ActionCreatorComposer<EA, string>;
+  apiOptions: ApiOptions<SA, EA, D, P>;
 }
+
+type RequestHeadersFragment = Pick<RequestInit, 'headers'>;
+type RequestBodyFragment = Pick<RequestInit, 'body'>;
 
 export function createAsyncAction<
   RA extends RequestAction<RC, P>,
@@ -54,35 +60,64 @@ export function createAsyncAction<
   makeSuccess,
   makeError,
   apiOptions,
-}: AsyncActionBuilderOptions<RA, SA, EA, D>): ActionCreator<ThunkAction<Promise<SA | EA>, D, P, SA | EA>> {
-  function getHeaders(contentType?: string): Record<string, string> {
-    const headers: Record<string, string> = {};
-
-    if (!contentType) {
-      headers['Content-Type'] = 'application/json';
-    } else {
-      headers['Content-Type'] = contentType;
+}: AsyncActionBuilderOptions<RA, SA, EA, D, P>): ActionCreator<ThunkAction<Promise<SA | EA>, D, P, SA | EA>> {
+  function getRequestUrl(baseUrl: string, payload: P, modifyUrl?: ApiRequestUrlModifier<P>): string {
+    if (modifyUrl) {
+      return modifyUrl(baseUrl, payload);
     }
 
-    return headers;
+    return baseUrl;
+  }
+
+  function getHeadersFragment(contentType?: string): RequestHeadersFragment {
+    const headersFragment: RequestHeadersFragment = {};
+
+    if (!contentType) {
+      headersFragment['headers'] = {
+        'Content-Type': 'application/json',
+      };
+    } else {
+      headersFragment['headers'] = {
+        'Content-Type': contentType,
+      };
+    }
+
+    return headersFragment;
+  }
+
+  function getBodyFragment(payload: P, constructBody?: ApiRequestBodyConstructor<P>): RequestBodyFragment {
+    if (constructBody) {
+      return { body: constructBody(payload) };
+    }
+
+    return {};
   }
 
   const request = makeRequest();
   const success = makeSuccess();
   const error = makeError();
 
-  const { url, method = 'GET', contentType = 'application/json', onSuccess, onError } = apiOptions;
+  const {
+    baseUrl,
+    method = 'GET',
+    contentType = 'application/json',
+    modifyUrl,
+    constructBody,
+    onSuccess,
+    onError,
+  } = apiOptions;
 
-  const headers = getHeaders(contentType);
-
-  return () => {
+  return (payload: P) => {
     return async (dispatch: Dispatch<RA | SA | EA>): Promise<SA | EA> => {
-      dispatch(request());
+      dispatch(request(payload));
+
+      const requestUrl = getRequestUrl(baseUrl, payload, modifyUrl);
 
       try {
-        const response = await fetch(url, {
+        const response = await fetch(requestUrl, {
           method,
-          headers,
+          ...getHeadersFragment(contentType),
+          ...getBodyFragment(payload, constructBody),
         });
 
         if (response.ok) {
@@ -97,9 +132,10 @@ export function createAsyncAction<
           const errorMessage = await onError(response, dispatch);
           return dispatch(error(errorMessage));
         }
-        return dispatch(error());
+
+        return dispatch(error('Unknown error occured'));
       } catch (err) {
-        return dispatch(error());
+        return dispatch(error('Failed to fetch data: server is not available'));
       }
     };
   };
