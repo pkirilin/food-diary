c18128296d4bc3ec0fb8502d14b3d6b5f6cc04f5
Pavel Kirilin <kirilin.pav@gmail.com>
2021-02-12 00:08:07 +0300

    Implemented editing and deleting notes

--- src/features/notes/components/NotesTableRow.tsx
+++ src/features/notes/components/NotesTableRow.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { IconButton, makeStyles, TableCell, TableRow, Tooltip } from '@material-ui/core';
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
-import { NoteItem } from '../models';
+import { NoteCreateEdit, NoteItem } from '../models';
+import { useDialog, useRouterId } from '../../__shared__/hooks';
+import NoteCreateEditDialog from './NoteCreateEditDialog';
+import { ConfirmationDialog } from '../../__shared__/components';
+import { deleteNote, editNote } from '../thunks';
 
 type NotesTableRowProps = {
   note: NoteItem;
@@ -19,20 +24,55 @@ const useStyles = makeStyles(theme => ({
 
 const NotesTableRow: React.FC<NotesTableRowProps> = ({ note }: NotesTableRowProps) => {
   const classes = useStyles();
+  const pageId = useRouterId('id');
+
+  const dispatch = useDispatch();
+
+  const noteEditDialog = useDialog<NoteCreateEdit>(noteData => {
+    dispatch(
+      editNote({
+        id: note.id,
+        note: { ...noteData },
+      }),
+    );
+  });
+
+  const noteDeleteDialog = useDialog(() => {
+    dispatch(deleteNote(note.id));
+  });
+
+  const handleEditClick = (): void => {
+    noteEditDialog.show();
+  };
+
+  const handleDeleteClick = (): void => {
+    noteDeleteDialog.show();
+  };
 
   return (
     <TableRow>
+      <NoteCreateEditDialog
+        {...noteEditDialog.binding}
+        mealType={note.mealType}
+        pageId={pageId}
+        note={note}
+      ></NoteCreateEditDialog>
+      <ConfirmationDialog
+        {...noteDeleteDialog.binding}
+        dialogTitle="Delete note"
+        dialogMessage={`Are you sure you want to delete this note: ${note.productName}, ${note.productQuantity} g, ${note.calories} cal?`}
+      ></ConfirmationDialog>
       <TableCell>{note.productName}</TableCell>
       <TableCell>{note.productQuantity}</TableCell>
       <TableCell>{note.calories}</TableCell>
       <TableCell className={classes.rowActions}>
         <Tooltip title="Edit note">
-          <IconButton size="small">
+          <IconButton size="small" onClick={handleEditClick}>
             <EditIcon fontSize="small"></EditIcon>
           </IconButton>
         </Tooltip>
         <Tooltip title="Delete note">
-          <IconButton size="small">
+          <IconButton size="small" onClick={handleDeleteClick}>
             <DeleteIcon fontSize="small"></DeleteIcon>
           </IconButton>
         </Tooltip>
