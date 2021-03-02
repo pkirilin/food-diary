ada0de2e4a478f60737b5c510c0efd25b7851239
Pavel Kirilin <kirilin.pav@gmail.com>
2021-03-02 22:25:58 +0300

    Removed old autocomplete hook

--- src/features/__shared__/hooks/index.ts
+++ src/features/__shared__/hooks/index.ts
@@ -1,20 +1,11 @@
 import { useDialog } from './useDialog';
 import { useInput } from './useInput';
-import { useInputAutocomplete } from './useInputAutocomplete';
 import { usePopover } from './usePopover';
 import { useRefreshEffect } from './useRefreshEffect';
 import { useRouterId } from './useRouterId';
 import useTypedSelector from './useTypedSelector';
 
-export {
-  useTypedSelector,
-  useInput,
-  useInputAutocomplete,
-  useDialog,
-  usePopover,
-  useRouterId,
-  useRefreshEffect,
-};
+export { useTypedSelector, useInput, useDialog, usePopover, useRouterId, useRefreshEffect };
 
 export * from './inputHooks';
 export * from './hookUtils';
