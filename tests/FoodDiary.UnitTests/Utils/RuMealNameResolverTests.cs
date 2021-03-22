44cd12014ab7481243265d4e96c7eb5b127ef17f
Pavel Kirilin <kirilin.pav@gmail.com>
2021-03-22 21:20:55 +0300

    Added tests for meal name resolver

---
+++ tests/FoodDiary.UnitTests/Utils/RuMealNameResolverTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using FoodDiary.Domain.Enums;
+using FoodDiary.Domain.Utils;
+using FoodDiary.Infrastructure.Utils;
+using Xunit;
+
+namespace FoodDiary.UnitTests.Utils
+{
+    public class RuMealNameResolverTests
+    {
+        private static IMealNameResolver Sut => new RuMealNameResolver();
+        
+        [Theory]
+        [InlineData(MealType.Breakfast, "Завтрак")]
+        [InlineData(MealType.SecondBreakfast, "Ланч")]
+        [InlineData(MealType.Lunch, "Обед")]
+        [InlineData(MealType.AfternoonSnack, "Полдник")]
+        [InlineData(MealType.Dinner, "Ужин")]
+        public void ShouldReturnMealNameByMealType(MealType mealType, string mealNameResult)
+        {
+            Sut.GetMealName(mealType).Should().Be(mealNameResult);
+        }
+    }
+}
