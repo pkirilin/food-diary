90a163ff3b9f288c4b4ed9677d1c91bc4090cab3
Pavel Kirilin <kirilin.pav@gmail.com>
2021-03-22 22:23:52 +0300

    Added page content title with current page date

--- src/features/pages/components/PageContent.tsx
+++ src/features/pages/components/PageContent.tsx
@@ -1,15 +1,17 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { Helmet } from 'react-helmet';
 import PageContentHeader from './PageContentHeader';
 import PageContentFooter from './PageContentFooter';
 import { MealsList } from '../../notes/components';
 import { getPageById } from '../thunks';
-import { useRouterId } from '../../__shared__/hooks';
+import { useRouterId, useTypedSelector } from '../../__shared__/hooks';
 import { getNotes } from '../../notes/thunks';
 
 const PageContent: React.FC = () => {
   const pageId = useRouterId('id');
   const dispatch = useDispatch();
+  const currentDate = useTypedSelector(state => state.pages.current?.date);
 
   useEffect(() => {
     dispatch(getPageById(pageId));
@@ -18,6 +20,13 @@ const PageContent: React.FC = () => {
 
   return (
     <React.Fragment>
+      <Helmet>
+        <title>
+          {currentDate
+            ? `Food diary | ${new Date(currentDate).toLocaleDateString()}`
+            : 'Food diary'}
+        </title>
+      </Helmet>
       <PageContentHeader></PageContentHeader>
       <MealsList></MealsList>
       <PageContentFooter></PageContentFooter>
