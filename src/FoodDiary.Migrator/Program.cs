f176f616f9d4f598fea0de69dfe4ce40b9c8625b
Pavel Kirilin <kirilin.pav@gmail.com>
2022-01-30 19:44:37 +0300

    Created migrator project

---
+++ src/FoodDiary.Migrator/Program.cs
@@ -0,0 +1,31 @@
+﻿using FoodDiary.Infrastructure;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.DependencyInjection;
+
+try
+{
+    var serviceProvider = BuildServiceProvider();
+    Migrate(serviceProvider);
+    return 0;
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    return -1;
+}
+
+IServiceProvider BuildServiceProvider()
+{
+    var services = new ServiceCollection();
+
+    return services.BuildServiceProvider();
+}
+
+void Migrate(IServiceProvider serviceProvider)
+{
+    using var serviceScope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope();
+    using var context = serviceScope.ServiceProvider.GetRequiredService<FoodDiaryContext>();
+    var migrator = context.GetInfrastructure().GetRequiredService<IMigrator>();
+    migrator.Migrate();
+}
