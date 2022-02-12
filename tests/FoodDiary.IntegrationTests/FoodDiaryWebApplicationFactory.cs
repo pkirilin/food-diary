c58cf83ce6a7f5b1a6ac757ef7d398eeb206afe1
Pavel Kirilin <kirilin.pav@gmail.com>
2022-02-12 17:24:38 +0300

    Switched tests for new WebApplicationFactory to SQLite

--- tests/FoodDiary.IntegrationTests/FoodDiaryWebApplicationFactory.cs
+++ tests/FoodDiary.IntegrationTests/FoodDiaryWebApplicationFactory.cs
@@ -1,11 +1,13 @@
 using System.IO;
 using FoodDiary.API;
 using FoodDiary.Infrastructure;
+using FoodDiary.IntegrationTests.Dsl.Builders;
 using FoodDiary.IntegrationTests.MockAuth;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,8 +17,17 @@ namespace FoodDiary.IntegrationTests;
 // ReSharper disable once ClassNeverInstantiated.Global
 public class FoodDiaryWebApplicationFactory : WebApplicationFactory<Startup>
 {
+    private SqliteConnection _connection;
+    private IServiceScope _scope;
+    private FoodDiaryContext _dbContext;
+    
+    public SeedDataForDbContextBuilder SeedDatabase() => new(DbContext);
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+        _connection = new SqliteConnection("Filename=:memory:");
+        _connection.Open();
+
         builder.UseEnvironment("Test");
 
         builder.ConfigureAppConfiguration((_, config) =>
@@ -29,13 +40,40 @@ public class FoodDiaryWebApplicationFactory : WebApplicationFactory<Startup>
 
         builder.ConfigureTestServices(services =>
         {
-            // TODO: replace InMemory provider with SQLite (https://docs.microsoft.com/en-us/ef/core/testing/testing-without-the-database#in-memory-provider)
-            services.AddDbContext<FoodDiaryContext>(options => options.UseInMemoryDatabase("FoodDiaryDatabase"));
+            services.AddDbContext<FoodDiaryContext>(
+                options => options.UseSqlite(_connection),
+                ServiceLifetime.Singleton);
             
             services.AddAuthentication("Test")
                 .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>("Test", _ =>
                 {
                 });
         });
+    }
+    
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            _connection?.Close();
+            _scope?.Dispose();
+        }
+        
+        base.Dispose(disposing);
+    }
+
+    private FoodDiaryContext DbContext
+    {
+        get
+        {
+            if (_dbContext != null)
+                return _dbContext;
+
+            var serviceScopeFactory = Services.GetRequiredService<IServiceScopeFactory>();
+            _scope = serviceScopeFactory.CreateScope();
+            _dbContext = _scope.ServiceProvider.GetRequiredService<FoodDiaryContext>();
+            _dbContext.Database.EnsureCreated();
+            return _dbContext;
+        }
     }
 }
