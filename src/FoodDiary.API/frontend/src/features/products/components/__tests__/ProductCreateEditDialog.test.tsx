129e27c9f182091718680034e8723a88c965b274
Pavel Kirilin <kirilin.pav@gmail.com>
2022-03-13 19:44:06 +0300

    tests: implemented 'category autocomplete starts loading options on open' case

--- src/FoodDiary.API/frontend/src/features/products/components/__tests__/ProductCreateEditDialog.test.tsx
+++ src/FoodDiary.API/frontend/src/features/products/components/__tests__/ProductCreateEditDialog.test.tsx
@@ -1,8 +1,30 @@
+import { render, screen, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import create from '../../../../test-utils';
 import ProductCreateEditDialog from '../ProductCreateEditDialog';
 
 describe('ProductCreateEditDialog', () => {
   describe('category autocomplete', () => {
-    test('starts loading options on open', () => {});
+    test('starts loading options on open', () => {
+      const ui = create
+        .component(
+          <ProductCreateEditDialog
+            open
+            onDialogConfirm={jest.fn()}
+            onDialogCancel={jest.fn()}
+          ></ProductCreateEditDialog>,
+        )
+        .withReduxStore()
+        .please();
+
+      render(ui);
+      const input = screen.getByRole('textbox', { name: /category/i });
+      userEvent.click(input);
+
+      const autocomplete = screen.getByRole('combobox', { expanded: true });
+      const loader = within(autocomplete).getByRole('progressbar');
+      expect(loader).toBeInTheDocument();
+    });
 
     test('displays all options on open', () => {});
 
