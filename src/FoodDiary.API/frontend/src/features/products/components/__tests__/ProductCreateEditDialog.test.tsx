4b69fdadbc6751ed2108e0280493694ef0985b9c
Pavel Kirilin <kirilin.pav@gmail.com>
2022-03-13 18:34:02 +0300

    tests: added test cases for category autocomplete

---
+++ src/FoodDiary.API/frontend/src/features/products/components/__tests__/ProductCreateEditDialog.test.tsx
@@ -0,0 +1,15 @@
+import ProductCreateEditDialog from '../ProductCreateEditDialog';
+
+describe('ProductCreateEditDialog', () => {
+  describe('category autocomplete', () => {
+    test('starts loading options on open', () => {});
+
+    test('displays all options on open', () => {});
+
+    test('displays loaded options matching input value', () => {});
+
+    test('displays empty message for empty autocomplete', () => {});
+
+    test('displays empty message if input value does not match any loaded option', () => {});
+  });
+});
