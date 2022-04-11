d55a4dfa45266dd07bbc5b574ca22b75037b1db4
Pavel Kirilin <kirilin.pav@gmail.com>
2022-04-11 21:57:20 +0300

    Refactored tests, added export menu case

--- src/FoodDiary.API/frontend/src/features/pages/components/__tests__/Pages.test.tsx
+++ src/FoodDiary.API/frontend/src/features/pages/components/__tests__/Pages.test.tsx
@@ -1,32 +1,53 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { ReactElement } from 'react';
 
 import { create } from 'src/test-utils';
 
 import Pages from '../Pages';
 
+let ui: ReactElement;
+
 describe('Pages', () => {
-  describe('when mounted', () => {
-    test('should render empty page items message if server has no data', () => {
-      const ui = create
+  describe('table', () => {
+    beforeEach(() => {
+      ui = create
         .component(<Pages></Pages>)
         .withReduxStore()
+        .withRouter()
         .please();
+    });
 
+    // TODO: fix this test, add waiting for spinner to disappear
+    test('shows empty page items message if server has no data', () => {
       render(ui);
 
       expect(screen.getByText(/no pages found/i)).toBeInTheDocument();
     });
 
-    test('should render page items if server has data', async () => {
-      const ui = create
+    test('shows page items if server has data', async () => {
+      render(ui);
+
+      await expect(screen).toContainPageItems('01.03.2022', '02.03.2022', '03.03.2022');
+    });
+  });
+
+  describe('export', () => {
+    beforeEach(() => {
+      ui = create
         .component(<Pages></Pages>)
         .withReduxStore()
         .withRouter()
         .please();
+    });
 
+    test('shows export menu on click', () => {
       render(ui);
 
-      await expect(screen).toContainPageItems('01.03.2022', '02.03.2022', '03.03.2022');
+      userEvent.click(screen.getByTitle('Export pages'));
+
+      // TODO
+      expect(false).toBeTruthy();
     });
   });
 });
