912d7098d95587e95e3db3d700e39b5ecb42bab7
Pavel Kirilin <kirilin.pav@gmail.com>
2022-04-17 22:38:21 +0300

    Added test for google docs export dialog

--- src/FoodDiary.API/frontend/src/features/pages/components/PagesToolbar/PagesToolbar.test.tsx
+++ src/FoodDiary.API/frontend/src/features/pages/components/PagesToolbar/PagesToolbar.test.tsx
@@ -19,3 +19,19 @@ test('shows export dialog when "export to json" clicked', () => {
   expect(exportDialog.getByText(/export pages/i)).toBeInTheDocument();
   expect(exportDialog.getByText(/export to json/i)).toBeInTheDocument();
 });
+
+test('shows export dialog when "export to google docs" clicked', () => {
+  const ui = create
+    .component(<PagesToolbar></PagesToolbar>)
+    .withReduxStore()
+    .withMuiPickersUtils()
+    .please();
+
+  render(ui);
+  userEvent.click(screen.getByTitle(/export pages/i));
+  userEvent.click(screen.getByText(/export to google docs/i));
+
+  const exportDialog = within(screen.getByRole('dialog'));
+  expect(exportDialog.getByText(/export pages/i)).toBeInTheDocument();
+  expect(exportDialog.getByText(/export to google docs/i)).toBeInTheDocument();
+});
