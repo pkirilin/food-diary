244bae151e5612070ed0167eaf4426a6b1a76a7a
Pavel Kirilin <kirilin.pav@gmail.com>
2022-05-05 15:12:10 +0300

    Implemented filling export document with page date and notes table

--- src/FoodDiary.Export.GoogleDocs/Implementation/GoogleDocsClient.cs
+++ src/FoodDiary.Export.GoogleDocs/Implementation/GoogleDocsClient.cs
@@ -10,29 +10,77 @@ internal class GoogleDocsClient : IGoogleDocsClient
     private const string ApplicationName = "food-diary";
 
     private readonly List<Request> _batchUpdateRequests = new();
+    private int _currentLocationIndex;
 
-    public Task<Document> CreateDocumentAsync(string title, string accessToken, CancellationToken cancellationToken)
+    public async Task<Document> CreateDocumentAsync(string title, string accessToken, CancellationToken cancellationToken)
     {
         var service = CreateService(accessToken);
-        
+
         var doc = new Document
         {
             Title = title
         };
         
-        return service.Documents
+        return await service.Documents
             .Create(doc)
             .ExecuteAsync(cancellationToken);
     }
 
     public void InsertH1Text(Document document, string text)
     {
-        throw new NotImplementedException();
+        _batchUpdateRequests.Add(new Request
+        {
+            InsertText = new InsertTextRequest
+            {
+                Text = text,
+                EndOfSegmentLocation = new EndOfSegmentLocation()
+            }
+        });
+
+        _currentLocationIndex += text.Length;
     }
 
     public void InsertTable(Document document, InsertTableOptions options)
     {
-        throw new NotImplementedException();
+        _currentLocationIndex++;
+
+        _batchUpdateRequests.Add(new Request
+        {
+            InsertTable = new InsertTableRequest
+            {
+                Rows = options.Cells.Count,
+                Columns = options.Cells.Max(c => c.Count),
+                Location = new Location { Index = _currentLocationIndex }
+            }
+        });
+        
+        _currentLocationIndex++;
+
+        foreach (var row in options.Cells)
+        {
+            _currentLocationIndex++;
+            
+            foreach (var cellText in row)
+            {
+                _currentLocationIndex += 2;
+
+                if (!string.IsNullOrWhiteSpace(cellText))
+                {
+                    _batchUpdateRequests.Add(new Request
+                    {
+                        InsertText = new InsertTextRequest
+                        {
+                            Text = cellText,
+                            Location = new Location { Index = _currentLocationIndex }
+                        }
+                    });
+
+                    _currentLocationIndex += cellText.Length;
+                }
+            }
+        }
+
+        _currentLocationIndex++;
     }
 
     public async Task BatchUpdateDocumentAsync(string documentId, string accessToken, CancellationToken cancellationToken)
@@ -45,6 +93,7 @@ internal class GoogleDocsClient : IGoogleDocsClient
             .ExecuteAsync(cancellationToken);
         
         _batchUpdateRequests.Clear();
+        _currentLocationIndex = 0;
     }
 
     private static DocsService CreateService(string accessToken)
