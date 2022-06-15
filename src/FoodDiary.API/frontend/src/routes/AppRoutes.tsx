f28c4313f639ac1f1f574ee1711a2ee7f12b5a53
Pavel Kirilin <kirilin.pav@gmail.com>
2022-06-15 22:05:51 +0300

    Refactored app routes on useRoutes hook

--- src/FoodDiary.API/frontend/src/routes/AppRoutes.tsx
+++ src/FoodDiary.API/frontend/src/routes/AppRoutes.tsx
@@ -1,48 +1,52 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Navigate, RouteObject, useRoutes } from 'react-router-dom';
 import { Auth, RequireAuth } from 'src/features/auth/components';
 import { PageContent, Pages } from 'src/features/pages/components';
 import { Products } from 'src/features/products/components';
 import { Categories } from 'src/features/categories/components';
 import NotFound from './NotFound';
 
+const PUBLIC_ROUTES: RouteObject[] = [
+  {
+    path: '/',
+    element: <Navigate to="/pages"></Navigate>,
+  },
+  {
+    path: '/auth',
+    element: <Auth></Auth>,
+  },
+];
+
+const PRIVATE_ROUTES: RouteObject[] = [
+  {
+    path: '/pages',
+    element: <Pages></Pages>,
+  },
+  {
+    path: '/pages/:id',
+    element: <PageContent></PageContent>,
+  },
+  {
+    path: '/products',
+    element: <Products></Products>,
+  },
+  {
+    path: '/categories',
+    element: <Categories></Categories>,
+  },
+];
+
 export default function AppRoutes() {
-  return (
-    <Routes>
-      <Route path="/auth" element={<Auth></Auth>}></Route>
-      <Route path="/" element={<Navigate to="/pages"></Navigate>}></Route>
-      <Route
-        path="/pages"
-        element={
-          <RequireAuth>
-            <Pages></Pages>
-          </RequireAuth>
-        }
-      ></Route>
-      <Route
-        path="/pages/:id"
-        element={
-          <RequireAuth>
-            <PageContent></PageContent>
-          </RequireAuth>
-        }
-      ></Route>
-      <Route
-        path="/products"
-        element={
-          <RequireAuth>
-            <Products></Products>
-          </RequireAuth>
-        }
-      ></Route>
-      <Route
-        path="/categories"
-        element={
-          <RequireAuth>
-            <Categories></Categories>
-          </RequireAuth>
-        }
-      ></Route>
-      <Route path="*" element={<NotFound></NotFound>}></Route>
-    </Routes>
-  );
+  const element = useRoutes([
+    ...PUBLIC_ROUTES,
+    ...PRIVATE_ROUTES.map(route => ({
+      ...route,
+      element: <RequireAuth>{route.element}</RequireAuth>,
+    })),
+    {
+      path: '*',
+      element: <NotFound></NotFound>,
+    },
+  ]);
+
+  return element;
 }
