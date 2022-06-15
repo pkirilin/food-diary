96c5aa4c69afbf9a1b544f7744338f84e69ae2a6
Pavel Kirilin <kirilin.pav@gmail.com>
2022-06-15 20:40:55 +0300

    Fixed '/auth' redirect when authenticated on initial load

--- src/FoodDiary.API/frontend/src/features/auth/hooks/useAuth.ts
+++ src/FoodDiary.API/frontend/src/features/auth/hooks/useAuth.ts
@@ -1,8 +1,4 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import { useAppSelector } from '../../__shared__/hooks';
-import { userLoggedIn } from '../auth.slice';
-import { getAccessToken } from '../cookie.service';
 
 export type UseAuthHookResult = {
   isAuthenticated: boolean;
@@ -10,15 +6,6 @@ export type UseAuthHookResult = {
 
 export default function useAuth(): UseAuthHookResult {
   const isAuthenticated = useAppSelector(state => state.auth.isAuthenticated);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const token = getAccessToken();
-
-    if (!isAuthenticated && token) {
-      dispatch(userLoggedIn());
-    }
-  }, [dispatch, isAuthenticated]);
 
   return { isAuthenticated };
 }
