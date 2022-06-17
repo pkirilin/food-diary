b3bd587d7d83cadd66b5ab9127e0170e842f7dcf
Pavel Kirilin <kirilin.pav@gmail.com>
2022-06-17 20:49:48 +0300

    Refactor: removed extra nesting and fixed test names

--- src/FoodDiary.API/frontend/src/App.test.tsx
+++ src/FoodDiary.API/frontend/src/App.test.tsx
@@ -2,34 +2,28 @@ import { render, screen } from '@testing-library/react';
 import { create } from 'src/test-utils';
 import App from './App';
 
-describe('App', () => {
-  describe('when user authenticated', () => {
-    test('should render navbar with pages list', () => {
-      const ui = create
-        .component(<App></App>)
-        .withReduxStore()
-        .withAuthToken('test_access_token')
-        .please();
+test('authenticated user can navigate and view private content', () => {
+  const ui = create
+    .component(<App></App>)
+    .withReduxStore()
+    .withAuthToken('test_access_token')
+    .please();
 
-      render(ui);
+  render(ui);
 
-      expect(screen.getByRole('navigation')).toBeInTheDocument();
-      expect(screen.getByRole('heading')).toBeInTheDocument();
-      expect(screen.getByRole('heading').textContent).toMatch(/Pages/);
-    });
-  });
+  expect(screen.getByRole('navigation')).toBeInTheDocument();
+  expect(screen.getByRole('heading')).toBeInTheDocument();
+  expect(screen.getByRole('heading').textContent).toMatch(/Pages/);
+});
 
-  describe('when user not authenticated', () => {
-    test('should render sign in page', () => {
-      const ui = create
-        .component(<App></App>)
-        .withReduxStore()
-        .withoutAuthToken()
-        .please();
+test('unauthenticated user must sign in to access private content', () => {
+  const ui = create
+    .component(<App></App>)
+    .withReduxStore()
+    .withoutAuthToken()
+    .please();
 
-      render(ui);
+  render(ui);
 
-      expect(screen.getByText(/Sign in/)).toBeInTheDocument();
-    });
-  });
+  expect(screen.getByText(/Sign in/)).toBeInTheDocument();
 });
