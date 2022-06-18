ecbdfcb2b54ffb1172f2eb848d91c3ffe52be2ae
Pavel Kirilin <kirilin.pav@gmail.com>
2022-06-18 23:39:58 +0300

    Removed adaptV4Theme

--- src/FoodDiary.API/frontend/src/theme.ts
+++ src/FoodDiary.API/frontend/src/theme.ts
@@ -1,28 +1,26 @@
-import { createTheme, adaptV4Theme } from '@mui/material';
+import { createTheme } from '@mui/material';
 import { green, lightGreen } from '@mui/material/colors';
 
-export default createTheme(
-  adaptV4Theme({
-    palette: {
-      mode: 'light',
-      primary: {
-        main: lightGreen[800],
-        contrastText: lightGreen[50],
-      },
-      secondary: {
-        main: green[100],
-        contrastText: green[900],
-      },
+export default createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: lightGreen[800],
+      contrastText: lightGreen[50],
     },
-    typography: {
-      h1: {
-        fontSize: '2rem',
-        fontWeight: 400,
-      },
-      h2: {
-        fontSize: '1.5rem',
-        fontWeight: 350,
-      },
+    secondary: {
+      main: green[100],
+      contrastText: green[900],
     },
-  }),
-);
+  },
+  typography: {
+    h1: {
+      fontSize: '2rem',
+      fontWeight: 400,
+    },
+    h2: {
+      fontSize: '1.5rem',
+      fontWeight: 350,
+    },
+  },
+});
