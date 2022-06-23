607cd0fbeedcd37cf8c3709bad7ba2b1603704e5
Pavel Kirilin <kirilin.pav@gmail.com>
2022-06-23 23:00:01 +0300

    Refactor: moved filter logic to custom hook

--- src/FoodDiary.API/frontend/src/features/pages/components/PagesFilter.tsx
+++ src/FoodDiary.API/frontend/src/features/pages/components/PagesFilter.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Box, Button, Paper } from '@mui/material';
 import dateFnsFormat from 'date-fns/format';
 import isValid from 'date-fns/isValid';
@@ -23,57 +23,65 @@ function validateFilterDate(date: Date | null) {
   return true;
 }
 
-const PagesFilter: React.FC = () => {
-  const classes = useFilterStyles();
+type DateChangedAction = typeof startDateChanged | typeof endDateChanged;
 
+function useFilter() {
+  const [isInitialized, setIsInitialized] = useState(false);
   const filterStartDate = useAppSelector(state => state.pages.filter.startDate || null);
   const filterEndDate = useAppSelector(state => state.pages.filter.endDate || null);
-  const filterChanged = useAppSelector(state => state.pages.filter.changed);
+  const isChanged = useAppSelector(state => state.pages.filter.changed);
   const dispatch = useAppDispatch();
-
-  const initialStartDate = filterStartDate ? new Date(filterStartDate) : null;
-  const initialEndDate = filterEndDate ? new Date(filterEndDate) : null;
-  const [startDate, setStartDate] = useState<Date | null>(initialStartDate);
-  const [endDate, setEndDate] = useState<Date | null>(initialEndDate);
-  const [isInitialized, setIsInitialized] = useState(false);
 
   useEffect(() => {
     setIsInitialized(true);
   }, []);
 
+  const initialStartDate = filterStartDate ? new Date(filterStartDate) : null;
+  const initialEndDate = filterEndDate ? new Date(filterEndDate) : null;
+
+  function reset() {
+    dispatch(filterReset());
+  }
+
+  const applyToDatePart = useCallback(
+    (date: Date | null, dateChangedAction: DateChangedAction) => {
+      if (!isInitialized) {
+        return;
+      }
+
+      if (!validateFilterDate(date)) {
+        return;
+      }
+
+      if (date === null) {
+        dispatch(dateChangedAction());
+        return;
+      }
+
+      dispatch(dateChangedAction(dateFnsFormat(date, 'yyyy-MM-dd')));
+    },
+    [dispatch, isInitialized],
+  );
+
+  return {
+    initialStartDate,
+    initialEndDate,
+    isChanged,
+    applyToDatePart,
+    reset,
+  };
+}
+
+const PagesFilter: React.FC = () => {
+  const classes = useFilterStyles();
+  const { initialStartDate, initialEndDate, isChanged, applyToDatePart, reset } = useFilter();
+  const [startDate, setStartDate] = useState<Date | null>(initialStartDate);
+  const [endDate, setEndDate] = useState<Date | null>(initialEndDate);
+
   useEffect(() => {
-    if (!isInitialized) {
-      return;
-    }
-
-    if (!validateFilterDate(startDate)) {
-      return;
-    }
-
-    if (startDate === null) {
-      dispatch(startDateChanged());
-      return;
-    }
-
-    dispatch(startDateChanged(dateFnsFormat(startDate, 'yyyy-MM-dd')));
-  }, [dispatch, isInitialized, startDate]);
-
-  useEffect(() => {
-    if (!isInitialized) {
-      return;
-    }
-
-    if (!validateFilterDate(endDate)) {
-      return;
-    }
-
-    if (endDate === null) {
-      dispatch(endDateChanged());
-      return;
-    }
-
-    dispatch(endDateChanged(dateFnsFormat(endDate, 'yyyy-MM-dd')));
-  }, [dispatch, isInitialized, endDate]);
+    applyToDatePart(startDate, startDateChanged);
+    applyToDatePart(endDate, endDateChanged);
+  }, [applyToDatePart, startDate, endDate]);
 
   return (
     <Box component={Paper} className={classes.root}>
@@ -90,13 +98,7 @@ const PagesFilter: React.FC = () => {
         onChange={value => setEndDate(value)}
       ></DatePicker>
       <Box className={classes.controls}>
-        <Button
-          variant="text"
-          disabled={!filterChanged}
-          onClick={() => {
-            dispatch(filterReset());
-          }}
-        >
+        <Button variant="text" disabled={!isChanged} onClick={reset}>
           Reset
         </Button>
       </Box>
