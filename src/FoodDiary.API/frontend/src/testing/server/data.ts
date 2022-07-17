836150ab448302d412095cb18eb1a35167cc4252
Pavel Kirilin <kirilin.pav@gmail.com>
2022-07-17 12:27:55 +0300

    Added temporary mock data layer for categories

---
+++ src/FoodDiary.API/frontend/src/testing/server/data.ts
@@ -0,0 +1,57 @@
+import { Category, CategoryCreateEdit } from 'src/features/categories';
+
+const INITIAL_CATEGORIES: Category[] = [
+  {
+    id: 1,
+    name: 'Bakery',
+    countProducts: 1,
+  },
+  {
+    id: 2,
+    name: 'Cereals',
+    countProducts: 5,
+  },
+  {
+    id: 3,
+    name: 'Dairy',
+    countProducts: 2,
+  },
+  {
+    id: 4,
+    name: 'Frozen Foods',
+    countProducts: 0,
+  },
+];
+
+let storedCategories: Category[] = INITIAL_CATEGORIES;
+
+const categories = {
+  get: () => storedCategories,
+
+  create: (category: CategoryCreateEdit) => {
+    storedCategories.push({
+      id: 5,
+      name: category.name,
+      countProducts: 0,
+    });
+  },
+
+  update: (id: number, payload: CategoryCreateEdit) => {
+    const categoryToUpdate = storedCategories.find(category => category.id === id);
+
+    storedCategories = [
+      ...storedCategories.filter(category => category.id !== id),
+      {
+        id,
+        name: payload.name,
+        countProducts: categoryToUpdate?.countProducts || 0,
+      },
+    ];
+  },
+
+  reset: () => {
+    storedCategories = INITIAL_CATEGORIES;
+  },
+};
+
+export { categories };
--- src/FoodDiary.API/frontend/src/testing/server/handlers/categories.ts
+++ src/FoodDiary.API/frontend/src/testing/server/handlers/categories.ts
@@ -1,40 +1,25 @@
 import { rest } from 'msw';
-import { Category } from 'src/features/categories';
 import config from 'src/features/__shared__/config';
-
-const MOCK_CATEGORIES: Category[] = [
-  {
-    id: 1,
-    name: 'Bakery',
-    countProducts: 1,
-  },
-  {
-    id: 2,
-    name: 'Cereals',
-    countProducts: 5,
-  },
-  {
-    id: 3,
-    name: 'Dairy',
-    countProducts: 2,
-  },
-  {
-    id: 4,
-    name: 'Frozen Foods',
-    countProducts: 0,
-  },
-];
+import { categories } from 'src/testing/server/data';
 
 export const categoriesHandlers = [
   rest.get(`${config.apiUrl}/api/v1/categories`, (req, res, ctx) => {
-    return res(ctx.json(MOCK_CATEGORIES));
+    return res(ctx.json(categories.get()));
   }),
 
   rest.post(`${config.apiUrl}/api/v1/categories`, (req, res, ctx) => {
+    categories.create({
+      name: 'New fancy category',
+    });
+
     return res(ctx.status(200));
   }),
 
   rest.put(`${config.apiUrl}/api/v1/categories/:id`, (req, res, ctx) => {
+    categories.update(1, {
+      name: 'Modified Bakery',
+    });
+
     return res(ctx.status(200));
   }),
 ];
