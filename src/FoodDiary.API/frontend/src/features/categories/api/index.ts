6db521e69c00f77f38287cec87e756ea666cfdb5
Pavel Kirilin <kirilin.pav@gmail.com>
2022-07-17 17:19:27 +0300

    Refactor: moved categoriesApi to index

--- src/FoodDiary.API/frontend/src/features/categories/api/index.ts
+++ src/FoodDiary.API/frontend/src/features/categories/api/index.ts
@@ -1,4 +1,53 @@
-import categoriesApi from './categoriesApi';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
+import { getToken } from 'src/features/auth';
+import config from 'src/features/__shared__/config';
+import { Category } from '../types';
+import { CreateCategoryRequest, DeleteCategoryRequest, EditCategoryRequest } from './contracts';
+
+const categoriesApi = createApi({
+  reducerPath: 'api.categories',
+
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${config.apiUrl}/api/v1/categories`,
+
+    prepareHeaders: headers => {
+      headers.append('Authorization', `Bearer ${getToken()}`);
+      return headers;
+    },
+  }),
+
+  endpoints: builder => ({
+    categories: builder.query<Category[], void>({
+      query: () => ({
+        method: 'GET',
+        url: '/',
+      }),
+    }),
+
+    createCategory: builder.mutation<void, CreateCategoryRequest>({
+      query: category => ({
+        method: 'POST',
+        url: '/',
+        body: category,
+      }),
+    }),
+
+    editCategory: builder.mutation<void, EditCategoryRequest>({
+      query: ({ id, name }) => ({
+        method: 'PUT',
+        url: `/${id}`,
+        body: { name },
+      }),
+    }),
+
+    deleteCategory: builder.mutation<void, DeleteCategoryRequest>({
+      query: id => ({
+        method: 'DELETE',
+        url: `/${id}`,
+      }),
+    }),
+  }),
+});
 
 export const {
   useCategoriesQuery,
