379fe5452a553222075562e27dc920e8e4dfac10
Pavel Kirilin <kirilin.pav@gmail.com>
2022-09-25 15:25:14 +0300

    Changed prod api url

--- src/FoodDiary.API/frontend/src/features/__shared__/config.ts
+++ src/FoodDiary.API/frontend/src/features/__shared__/config.ts
@@ -1,6 +1,6 @@
 const apiUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://food-diary-webapp.herokuapp.com'
+    ? 'https://food-diary.azurewebsites.net'
     : 'https://localhost:5001';
 
 const googleClientId = '772368064111-19hqh3c6ksu56ke45nm24etn7qoma88v.apps.googleusercontent.com';
