2a8d3091525cb2721a3b1f274876c42bc2e6c96b
Pavel Kirilin <kirilin.pav@gmail.com>
2022-10-05 22:09:12 +0300

    Setup AppSelect test environment

--- src/FoodDiary.API/frontend/src/components/AppSelect/AppSelect.test.tsx
+++ src/FoodDiary.API/frontend/src/components/AppSelect/AppSelect.test.tsx
@@ -1,17 +1,76 @@
-export {};
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { render } from 'src/testing';
+import { AutocompleteOption } from 'src/types';
+import AppSelect from './AppSelect';
 
-test('all options are visible after clicking on input', () => {});
+const OPTIONS: AutocompleteOption[] = [
+  {
+    id: 1,
+    name: 'John',
+  },
+  {
+    id: 2,
+    name: 'Peter',
+  },
+  {
+    id: 3,
+    name: 'Kate',
+  },
+];
 
-test('all options are visible if closed with filtered options and then opened again', () => {});
+const AppSelectTest: React.FC = () => {
+  function getDisplayName({ name }: AutocompleteOption) {
+    return name;
+  }
 
-test('options can be marked as loading', () => {});
+  function areOptionsEqual(first: AutocompleteOption, second: AutocompleteOption) {
+    return first.name === second.name;
+  }
 
-test('visible options match input value', () => {});
+  return (
+    <AppSelect
+      availableOptions={OPTIONS}
+      getDisplayName={getDisplayName}
+      areOptionsEqual={areOptionsEqual}
+      label="Name"
+      placeholder="Select name"
+    />
+  );
+};
 
-test('initializes selected value if it specified', () => {});
+test('all options are visible after clicking on input', async () => {
+  render(<AppSelectTest />);
 
-test('no options are visible if input value does not match any existing option', () => {});
+  await userEvent.click(screen.getByPlaceholderText(/select name/i));
 
-test('no options are visible after input is closed', () => {});
+  expect(screen).toContainOptions('John', 'Peter', 'Kate');
+});
 
-test('no options are visible after clicking on input if autocomplete has no options', () => {});
+test('all options are visible if closed with filtered options and then opened again', () => {
+  expect(false).toBeTruthy();
+});
+
+test('options can be marked as loading', () => {
+  expect(false).toBeTruthy();
+});
+
+test('visible options match input value', () => {
+  expect(false).toBeTruthy();
+});
+
+test('initializes selected value if it specified', () => {
+  expect(false).toBeTruthy();
+});
+
+test('no options are visible if input value does not match any existing option', () => {
+  expect(false).toBeTruthy();
+});
+
+test('no options are visible after input is closed', () => {
+  expect(false).toBeTruthy();
+});
+
+test('no options are visible after clicking on input if autocomplete has no options', () => {
+  expect(false).toBeTruthy();
+});
--- src/FoodDiary.API/frontend/src/components/AppSelect/AppSelect.tsx
+++ src/FoodDiary.API/frontend/src/components/AppSelect/AppSelect.tsx
@@ -1,5 +1,46 @@
-const AppSelect = () => {
-  return <div>AppSelect</div>;
+import { Autocomplete, CircularProgress, TextField } from '@mui/material';
+import React from 'react';
+
+type AppSelectProps<TValue> = {
+  availableOptions: TValue[];
+  getDisplayName: (value: TValue) => string;
+  areOptionsEqual: (first: TValue, second: TValue) => boolean;
+  label?: string;
+  placeholder?: string;
+  isLoading?: boolean;
+};
+
+const AppSelect = <TOption,>({
+  availableOptions,
+  getDisplayName,
+  areOptionsEqual,
+  label,
+  placeholder,
+  isLoading,
+}: AppSelectProps<TOption>): React.ReactElement => {
+  return (
+    <Autocomplete
+      options={availableOptions}
+      getOptionLabel={getDisplayName}
+      isOptionEqualToValue={areOptionsEqual}
+      renderInput={params => (
+        <TextField
+          {...params}
+          label={label}
+          placeholder={placeholder}
+          margin="normal"
+          InputProps={{
+            ...params.InputProps,
+            endAdornment: isLoading ? (
+              <CircularProgress color="inherit" size={20} />
+            ) : (
+              params.InputProps.endAdornment
+            ),
+          }}
+        />
+      )}
+    />
+  );
 };
 
 export default AppSelect;
