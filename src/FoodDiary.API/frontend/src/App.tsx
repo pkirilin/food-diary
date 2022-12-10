b166f730eee580a95f6be83e4dddc4aa98f7c878
Pavel Kirilin <kirilin.pav@gmail.com>
2022-12-10 19:20:59 +0300

    Added mobile menu drawer

--- src/FoodDiary.API/frontend/src/App.tsx
+++ src/FoodDiary.API/frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import { Box } from '@mui/material';
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { Logout } from './features/auth/components';
 import { useAuth } from './features/auth/hooks';
-import Navbar from './Navbar';
+import { NavigationBar } from './features/navigation';
 import { AppRoutes } from './routes';
 
 const App: React.FC = () => {
@@ -13,7 +14,7 @@ const App: React.FC = () => {
       <Helmet>
         <title>Food diary</title>
       </Helmet>
-      {isAuthenticated && <Navbar />}
+      {isAuthenticated && <NavigationBar renderLogout={() => <Logout />} />}
       <Box component="main" position="relative">
         <AppRoutes />
       </Box>
--- src/FoodDiary.API/frontend/src/features/navigation/components/NavigationBar.tsx
+++ src/FoodDiary.API/frontend/src/features/navigation/components/NavigationBar.tsx
@@ -1,6 +1,21 @@
+import CloseIcon from '@mui/icons-material/Close';
 import MenuIcon from '@mui/icons-material/Menu';
-import { AppBar, Container, IconButton, styled, Toolbar } from '@mui/material';
-import React from 'react';
+import {
+  AppBar,
+  Box,
+  Container,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  styled,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import React, { useState } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 
 const StyledToolbar = styled(Toolbar)(({ theme }) => ({
@@ -18,23 +33,81 @@ const StyledNavBrandLink = styled(RouterLink)(({ theme }) => ({
   fontWeight: 'bold',
 }));
 
+const StyledDrawer = styled(Drawer)(({ theme }) => ({
+  display: 'block',
+
+  '& .MuiDrawer-paper': {
+    width: '240px',
+  },
+
+  [theme.breakpoints.up('sm')]: {
+    display: 'none',
+  },
+}));
+
 type NavigationBarProps = {
   renderLogout: () => React.ReactElement;
 };
 
 const NavigationBar: React.FC<NavigationBarProps> = ({ renderLogout }) => {
+  const [isDrawerOpened, setIsDrawerOpened] = useState(false);
+
+  function handleDrawerToggle() {
+    setIsDrawerOpened(isOpened => !isOpened);
+  }
+
   return (
-    <AppBar position="static">
-      <Container>
-        <StyledToolbar disableGutters id="back-to-top-anchor">
-          <IconButton size="large" edge="start" color="inherit" aria-label="menu">
-            <MenuIcon />
+    <React.Fragment>
+      <AppBar position="static">
+        <Container>
+          <StyledToolbar disableGutters id="back-to-top-anchor">
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="Open menu"
+              onClick={handleDrawerToggle}
+            >
+              <MenuIcon />
+            </IconButton>
+            <StyledNavBrandLink to="/">Food diary</StyledNavBrandLink>
+            {renderLogout()}
+          </StyledToolbar>
+        </Container>
+      </AppBar>
+      <Box component="nav">
+        <StyledDrawer
+          variant="temporary"
+          open={isDrawerOpened}
+          onClose={handleDrawerToggle}
+          ModalProps={{ keepMounted: true }}
+        >
+          <Typography component={'div'}>Food diary</Typography>
+          <Divider sx={{ width: '100%' }} />
+          <List>
+            <ListItem key="one" disablePadding>
+              <ListItemButton>
+                <ListItemText primary={'one'} />
+              </ListItemButton>
+            </ListItem>
+            <ListItem key="two" disablePadding>
+              <ListItemButton>
+                <ListItemText primary={'two'} />
+              </ListItemButton>
+            </ListItem>
+          </List>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="Close menu"
+            onClick={handleDrawerToggle}
+          >
+            <CloseIcon />
           </IconButton>
-          <StyledNavBrandLink to="/">Food diary</StyledNavBrandLink>
-          {renderLogout()}
-        </StyledToolbar>
-      </Container>
-    </AppBar>
+        </StyledDrawer>
+      </Box>
+    </React.Fragment>
   );
 };
 
