985de94fa756469bda5559e37c38ed203568e3dd
Pavel Kirilin <kirilin.pav@gmail.com>
2022-12-16 21:46:59 +0300

    Removed old navbar

--- src/FoodDiary.API/frontend/src/components/index.ts
+++ src/FoodDiary.API/frontend/src/components/index.ts
@@ -1,10 +1,9 @@
 import AppButton from './AppButton';
 import AppFab from './AppFab';
 import AppLinearProgress from './AppLinearProgress';
-import AppNavLink from './AppNavLink';
 import AppSelect from './AppSelect';
 import DatePicker from './DatePicker';
 
-export { AppButton, AppFab, AppLinearProgress, AppNavLink, AppSelect, DatePicker };
+export { AppButton, AppFab, AppLinearProgress, AppSelect, DatePicker };
 
 export * from 'src/features/__shared__/components';
