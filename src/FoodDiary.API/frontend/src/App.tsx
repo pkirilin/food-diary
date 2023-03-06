f81c7230ec7e57b24180a8e4150929040f92cc33
Pavel Kirilin <kirilin.pav@gmail.com>
2023-03-06 21:14:49 +0300

    Moved navigation bar inside routes

--- src/FoodDiary.API/frontend/src/App.tsx
+++ src/FoodDiary.API/frontend/src/App.tsx
@@ -1,19 +1,14 @@
 import { Box } from '@mui/material';
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { useAuth } from './features/auth';
-import { NavigationBar } from './features/navigation';
 import { AppRoutes } from './routes';
 
 const App: React.FC = () => {
-  const { user } = useAuth();
-
   return (
     <React.Fragment>
       <Helmet>
         <title>Food diary</title>
       </Helmet>
-      {user?.isAuthenticated && <NavigationBar />}
       <Box component="main" position="relative">
         <AppRoutes />
       </Box>
--- src/FoodDiary.API/frontend/src/routes/AppRoutes.tsx
+++ src/FoodDiary.API/frontend/src/routes/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { Navigate, RouteObject, useRoutes } from 'react-router-dom';
 import { Login, PostLogin, PostLogout, RequireAuth } from 'src/features/auth';
 import { Categories } from 'src/features/categories';
+import { NavigationBar } from 'src/features/navigation';
 import { Pages } from 'src/features/pages';
 import { PageContent } from 'src/features/pages/components';
 import { Products } from 'src/features/products';
@@ -49,7 +50,12 @@ export default function AppRoutes() {
     ...PUBLIC_ROUTES,
     ...PRIVATE_ROUTES.map(route => ({
       ...route,
-      element: <RequireAuth>{route.element}</RequireAuth>,
+      element: (
+        <RequireAuth>
+          <NavigationBar />
+          {route.element}
+        </RequireAuth>
+      ),
     })),
     {
       path: '*',
