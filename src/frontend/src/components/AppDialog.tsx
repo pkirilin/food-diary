34afccdaaaf36bbbca53b6cfbccdb711d480753f
Pavel Kirilin <kirilin.pav@gmail.com>
2023-10-28 11:50:45 +0300

    Added reusable dialog with fixed styles

---
+++ src/frontend/src/components/AppDialog.tsx
@@ -0,0 +1,55 @@
+import CloseIcon from '@mui/icons-material/Close';
+import { Dialog, DialogTitle, DialogContent, DialogActions, IconButton } from '@mui/material';
+import { FC, ReactElement } from 'react';
+
+type AppDialogProps = {
+  title: string;
+  isOpened: boolean;
+  onClose: () => void;
+  content: ReactElement;
+  actionSubmit: ReactElement;
+  actionCancel?: ReactElement;
+};
+
+const AppDialog: FC<AppDialogProps> = ({
+  title,
+  isOpened,
+  onClose,
+  content,
+  actionSubmit,
+  actionCancel,
+}) => {
+  return (
+    <Dialog open={isOpened} onClose={onClose} fullWidth>
+      <IconButton
+        aria-label="close"
+        onClick={onClose}
+        sx={{
+          position: 'absolute',
+          top: theme => theme.spacing(1),
+          right: theme => theme.spacing(2),
+          color: theme => theme.palette.grey[500],
+        }}
+      >
+        <CloseIcon />
+      </IconButton>
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent>{content}</DialogContent>
+      <DialogActions
+        disableSpacing
+        sx={theme => ({
+          padding: `0 ${theme.spacing(3)} ${theme.spacing(2)}`,
+
+          '& > :not(:first-child)': {
+            marginLeft: theme.spacing(2),
+          },
+        })}
+      >
+        {actionCancel}
+        {actionSubmit}
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default AppDialog;
--- src/frontend/src/components/index.ts
+++ src/frontend/src/components/index.ts
@@ -1,9 +1,10 @@
 import AppButton from './AppButton';
+import AppDialog from './AppDialog';
 import AppFab from './AppFab';
 import AppLinearProgress from './AppLinearProgress';
 import AppSelect from './AppSelect';
 import DatePicker from './DatePicker';
 
-export { AppButton, AppFab, AppLinearProgress, AppSelect, DatePicker };
+export { AppButton, AppDialog, AppFab, AppLinearProgress, AppSelect, DatePicker };
 
 export * from 'src/features/__shared__/components';
