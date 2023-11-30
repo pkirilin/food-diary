f91e911e95a24311601d0c2e22e06bb43d46d11b
Pavel Kirilin <kirilin.pav@gmail.com>
2023-11-30 23:37:19 +0300

    Configured top level awaits

--- src/frontend/src/index.tsx
+++ src/frontend/src/index.tsx
@@ -7,21 +7,19 @@ import { GOOGLE_ANALYTICS_ENABLED, MSW_ENABLED } from './config';
 import { initGoogleAnalytics } from './googleAnalytics';
 import store from './store';
 
-(async () => {
-  if (MSW_ENABLED) {
-    await initBrowserMockApi();
-  }
+if (MSW_ENABLED) {
+  await initBrowserMockApi();
+}
 
-  if (GOOGLE_ANALYTICS_ENABLED) {
-    initGoogleAnalytics();
-  }
+if (GOOGLE_ANALYTICS_ENABLED) {
+  initGoogleAnalytics();
+}
 
-  const container = document.getElementById('root') as HTMLElement;
-  const root = createRoot(container);
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container);
 
-  root.render(
-    <AppProvider store={store}>
-      <App />
-    </AppProvider>,
-  );
-})();
+root.render(
+  <AppProvider store={store}>
+    <App />
+  </AppProvider>,
+);
