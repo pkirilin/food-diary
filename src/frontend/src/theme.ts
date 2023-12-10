3b3084aea11790e7e5db71dc59b5857f65c08667
Pavel Kirilin <kirilin.pav@gmail.com>
2023-12-10 20:14:12 +0300

    Fixed theme module augmentation

--- src/frontend/src/theme.ts
+++ src/frontend/src/theme.ts
@@ -1,6 +1,14 @@
 import { createTheme } from '@mui/material';
 import { green } from '@mui/material/colors';
 
+declare module '@mui/material/styles' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  interface Theme {}
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  interface ThemeOptions {}
+}
+
 export default createTheme({
   palette: {
     mode: 'light',
