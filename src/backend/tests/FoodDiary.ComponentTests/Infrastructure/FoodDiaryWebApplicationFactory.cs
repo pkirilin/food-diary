6adc662b5e89d95ad55ecfe035e565ba83269f85
Pavel Kirilin <kirilin.pav@gmail.com>
2023-12-17 23:23:56 +0300

    Added component tests configuration

--- src/backend/tests/FoodDiary.ComponentTests/Infrastructure/FoodDiaryWebApplicationFactory.cs
+++ src/backend/tests/FoodDiary.ComponentTests/Infrastructure/FoodDiaryWebApplicationFactory.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Testcontainers.PostgreSql;
 
@@ -15,6 +16,8 @@ namespace FoodDiary.ComponentTests.Infrastructure;
 [UsedImplicitly]
 public class FoodDiaryWebApplicationFactory : WebApplicationFactory<Startup>, IAsyncLifetime
 {
+    private const string EnvironmentName = "ComponentTests";
+    
     private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
         .WithImage("postgres:12.2-alpine")
         .WithUsername("postgres")
@@ -24,6 +27,16 @@ public class FoodDiaryWebApplicationFactory : WebApplicationFactory<Startup>, IA
     
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+        builder.UseEnvironment(EnvironmentName);
+        
+        builder.ConfigureAppConfiguration(configurationBuilder =>
+        {
+            configurationBuilder
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{EnvironmentName}.json", false, false);
+        });
+        
         builder.ConfigureTestServices(services =>
         {
             var dbContextOptionsDescriptor = services
