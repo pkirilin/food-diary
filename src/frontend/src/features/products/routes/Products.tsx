44aea972b75feddd08f775381ebfdd1075b3c66d
Pavel Kirilin <kirilin.pav@gmail.com>
2023-12-31 16:56:22 +0300

    Clear products filter on route change

--- src/frontend/src/features/products/routes/Products.tsx
+++ src/frontend/src/features/products/routes/Products.tsx
@@ -1,6 +1,6 @@
 import { Box, Container, Paper, Typography } from '@mui/material';
 import { visuallyHidden } from '@mui/utils';
-import { type FC } from 'react';
+import { useEffect, type FC } from 'react';
 import { useAppDispatch } from 'src/hooks';
 import { useAppSelector } from 'src/store';
 import { productsApi } from '../api';
@@ -8,14 +8,23 @@ import ProductsTable from '../components/ProductsTable';
 import ProductsTablePagination from '../components/ProductsTablePagination';
 import ProductsTableToolbar from '../components/ProductsTableToolbar';
 import { selectProductsQueryArg, selectCheckedProductIds } from '../selectors';
-import { productsUnchecked, productsChecked } from '../store';
+import { productsUnchecked, productsChecked, filterReset } from '../store';
 import { type Product } from '../types';
 
 const Products: FC = () => {
   const getProductsQueryArg = useAppSelector(selectProductsQueryArg);
   const getProductsQuery = productsApi.useGetProductsQuery(getProductsQueryArg);
   const checkedProductIds = useAppSelector(selectCheckedProductIds);
+  const filterChanged = useAppSelector(state => state.products.filter.changed);
   const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    return () => {
+      if (filterChanged) {
+        dispatch(filterReset());
+      }
+    };
+  }, [dispatch, filterChanged]);
 
   const handleCheckedProductsChange = (products: Product[], newCheckedIds: number[]): void => {
     if (newCheckedIds.length > 0) {
