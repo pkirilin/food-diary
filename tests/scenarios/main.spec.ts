bb4747a5852fce0153433751db75f5cc18d72532
Pavel Kirilin <kirilin.pav@gmail.com>
2024-01-02 17:08:21 +0300

    Created playwright project

---
+++ tests/scenarios/main.spec.ts
@@ -0,0 +1,6 @@
+import { expect, test } from "@playwright/test";
+
+test("should display google search page", async ({ page }) => {
+  await page.goto("https://www.google.com/");
+  await expect(page.getByRole("img", { name: "Google" })).toBeVisible();
+});
