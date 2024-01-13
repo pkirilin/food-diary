c8e0cfcc7a12f4fc842479365754030e60a049f4
Pavel Kirilin <kirilin.pav@gmail.com>
2024-01-13 15:53:24 +0300

    Renamed 'auth/profile' to 'auth/status'

--- src/frontend/tests/mockApi/auth/auth.handlers.ts
+++ src/frontend/tests/mockApi/auth/auth.handlers.ts
@@ -1,11 +1,11 @@
 import { http, type HttpHandler, HttpResponse } from 'msw';
 import { API_URL, FAKE_AUTH_ENABLED, FAKE_AUTH_LOGIN_ON_INIT } from 'src/config';
-import { type AuthProfileResponse } from 'src/features/auth';
+import { type GetAuthStatusResponse } from 'src/features/auth';
 
 export const handlers: HttpHandler[] = [
-  http.get(`${API_URL}/api/v1/auth/profile`, () =>
-    HttpResponse.json({
+  http.get(`${API_URL}/api/v1/auth/status`, () =>
+    HttpResponse.json<GetAuthStatusResponse>({
       isAuthenticated: FAKE_AUTH_ENABLED && FAKE_AUTH_LOGIN_ON_INIT,
-    } satisfies AuthProfileResponse),
+    }),
   ),
 ];
