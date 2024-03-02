77bc28f2570747d83f0b852ee92dd787ca1612d1
Pavel Kirilin <kirilin.pav@gmail.com>
2024-03-02 17:13:23 +0300

    Fixed loading button styles

--- src/frontend/src/shared/ui/Button/Button.tsx
+++ src/frontend/src/shared/ui/Button/Button.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   CircularProgress,
   Button as MuiButton,
   type ButtonProps as MuiButtonProps,
@@ -9,13 +10,9 @@ interface Props extends MuiButtonProps {
   loading?: boolean;
 }
 
-export const Button: FC<Props> = ({ children, loading, disabled, color, startIcon, ...props }) => (
-  <MuiButton
-    {...props}
-    disabled={!!disabled || loading}
-    color={color}
-    startIcon={loading ? <CircularProgress size={16} color={color} /> : startIcon}
-  >
-    {children}
+export const Button: FC<Props> = ({ children, loading, disabled, color, ...props }) => (
+  <MuiButton {...props} disabled={!!disabled || loading} color={color}>
+    {loading && <CircularProgress size={16} color={color} sx={{ position: 'absolute' }} />}
+    <Box visibility={loading ? 'hidden' : 'visible'}>{children}</Box>
   </MuiButton>
 );
