d954ccdf25f77505cfe1e276c614b9ca425b0ca8
Pavel Kirilin <kirilin.pav@gmail.com>
2024-03-05 18:57:41 +0300

    Removed useFilterStyles

--- src/frontend/src/features/__shared__/styles.ts
+++ src/frontend/src/features/__shared__/styles.ts
@@ -1,21 +1,5 @@
 import makeStyles from '@mui/styles/makeStyles';
 
-export const useFilterStyles = makeStyles(theme => ({
-  root: {
-    width: '450px',
-    padding: theme.spacing(2),
-  },
-  controls: {
-    display: 'flex',
-    justifyContent: 'flex-end',
-    marginTop: theme.spacing(2),
-
-    '& > :not(:first-child)': {
-      marginLeft: theme.spacing(1),
-    },
-  },
-}));
-
 export const useFilterAppliedParamsStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
