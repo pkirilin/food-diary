20811f56ab072c06bb9873f4fd93fe46e2ee9c29
Pavel Kirilin <kirilin.pav@gmail.com>
2024-03-09 21:37:38 +0300

    Removed unused code

--- src/backend/src/FoodDiary.Domain/Abstractions/v2/IFoodDiaryUnitOfWork.cs
+++ src/backend/src/FoodDiary.Domain/Abstractions/v2/IFoodDiaryUnitOfWork.cs
@@ -1,16 +1,10 @@
-using System.Threading;
-using System.Threading.Tasks;
 using FoodDiary.Domain.Repositories.v2;
 
 namespace FoodDiary.Domain.Abstractions.v2;
 
 public interface IFoodDiaryUnitOfWork
 {
-    IPagesRepository Pages { get; }
-    
     IProductsRepository Products { get; }
     
     ICategoriesRepository Categories { get; }
-    
-    Task SaveChangesAsync(CancellationToken cancellationToken);
 }
