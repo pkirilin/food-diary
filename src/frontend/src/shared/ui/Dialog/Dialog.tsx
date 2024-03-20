eeaacf9a6f87853836789357ecda4d47de34adc4
Pavel Kirilin <kirilin.pav@gmail.com>
2024-03-20 17:29:26 +0300

    Fixed Dialog crash on screen size change

--- src/frontend/src/shared/ui/Dialog/Dialog.tsx
+++ src/frontend/src/shared/ui/Dialog/Dialog.tsx
@@ -1,5 +1,5 @@
 import { type Theme, useMediaQuery } from '@mui/material';
-import { lazy, type FC } from 'react';
+import { lazy, type FC, Suspense } from 'react';
 import { type RenderActionFn, type DialogBaseProps } from './types';
 
 type RenderMode = 'modal' | 'fullScreenOnMobile';
@@ -16,9 +16,13 @@ const ModalDialog = lazy(() => import('./ModalDialog'));
 export const Dialog: FC<Props> = ({ renderMode = 'modal', renderCancel, ...props }) => {
   const isMobile = useMediaQuery<Theme>(theme => theme.breakpoints.down('md'));
 
-  return isMobile && renderMode === 'fullScreenOnMobile' ? (
-    <FullScreenDialog {...props} />
-  ) : (
-    <ModalDialog {...props} renderCancel={renderCancel} />
+  return (
+    <Suspense>
+      {isMobile && renderMode === 'fullScreenOnMobile' ? (
+        <FullScreenDialog {...props} />
+      ) : (
+        <ModalDialog {...props} renderCancel={renderCancel} />
+      )}
+    </Suspense>
   );
 };
