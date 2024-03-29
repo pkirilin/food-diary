9daf1668c07e3e3ae86214d8ec8b5eec6f63a65d
Pavel Kirilin <kirilin.pav@gmail.com>
2024-03-29 12:30:19 +0300

    Added menu icons

--- src/frontend/src/widgets/navbar/ui/MenuList/MenuList.tsx
+++ src/frontend/src/widgets/navbar/ui/MenuList/MenuList.tsx
@@ -1,13 +1,34 @@
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import CategoryIcon from '@mui/icons-material/Category';
+import RestaurantIcon from '@mui/icons-material/Restaurant';
 import { List } from '@mui/material';
 import { Box } from '@mui/system';
 import { type FC } from 'react';
-import { NAV_LINKS } from '../../lib';
 import { MenuListItem } from './MenuListItem';
+import { type NavLink } from './types';
+
+const NAV_LINKS: NavLink[] = [
+  {
+    icon: <CalendarMonthIcon />,
+    title: 'Pages',
+    path: '/pages',
+  },
+  {
+    icon: <RestaurantIcon />,
+    title: 'Products',
+    path: '/products',
+  },
+  {
+    icon: <CategoryIcon />,
+    title: 'Categories',
+    path: '/categories',
+  },
+];
 
 export const MenuList: FC = () => (
   <Box component={List} disablePadding>
-    {NAV_LINKS.map(({ title, path }, index) => (
-      <MenuListItem key={`${index}-${title}`} title={title} path={path} />
+    {NAV_LINKS.map((navLink, index) => (
+      <MenuListItem key={`${index}-${navLink.title}`} navLink={navLink} />
     ))}
   </Box>
 );
--- src/frontend/src/widgets/navbar/ui/MenuList/MenuListItem.tsx
+++ src/frontend/src/widgets/navbar/ui/MenuList/MenuListItem.tsx
@@ -1,22 +1,21 @@
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import { Box, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import { type FC } from 'react';
 import { Link as RouterLink, useMatch } from 'react-router-dom';
+import { type NavLink } from './types';
 
 interface Props {
-  title: string;
-  path: string;
+  navLink: NavLink;
 }
 
-export const MenuListItem: FC<Props> = ({ title, path }) => {
-  const match = useMatch(path);
+export const MenuListItem: FC<Props> = ({ navLink }) => {
+  const match = useMatch(navLink.path);
   const active = !!match;
 
   return (
     <ListItem disablePadding>
       <ListItemButton
         component={RouterLink}
-        to={active ? '' : path}
+        to={active ? '' : navLink.path}
         selected={active}
         disableTouchRipple={active}
         sx={theme => ({
@@ -26,10 +25,8 @@ export const MenuListItem: FC<Props> = ({ title, path }) => {
           },
         })}
       >
-        <Box component={ListItemIcon}>
-          <CalendarMonthIcon />
-        </Box>
-        <ListItemText primary={title} />
+        <Box component={ListItemIcon}>{navLink.icon}</Box>
+        <ListItemText primary={navLink.title} />
       </ListItemButton>
     </ListItem>
   );
