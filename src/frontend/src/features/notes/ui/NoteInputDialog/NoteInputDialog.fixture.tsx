4af97320d29378d28449a0c0c4b9753f90a33210
Pavel Kirilin <kirilin.pav@gmail.com>
2024-05-03 21:06:15 +0300

    Added tests new NoteInputDialog

--- src/frontend/src/features/notes/ui/NoteInputDialog/NoteInputDialog.fixture.tsx
+++ src/frontend/src/features/notes/ui/NoteInputDialog/NoteInputDialog.fixture.tsx
@@ -6,11 +6,13 @@ import { type Mock } from 'vitest';
 import { type productsModel } from '@/entities/products';
 import { MealType, type NoteCreateEdit, NoteInputDialog } from '@/features/notes';
 import theme from '@/theme';
+import { type SelectOption } from '@/types';
 import { WithTriggerButton } from '@tests/sideEffects';
 import { type ProductSelectOption } from 'src/features/products';
 
 class NoteInputDialogBuilder {
   private readonly _products: ProductSelectOption[] = [];
+  private readonly _categories: SelectOption[] = [];
   private _selectedProductName: string | null = null;
   private _quantity: number = 100;
   private _onSubmitMock: Mock = vi.fn();
@@ -23,7 +25,7 @@ class NoteInputDialogBuilder {
             <NoteInputDialog
               opened={active}
               onClose={onTriggerClick}
-              title="Test"
+              title="Note"
               submitText="Submit"
               mealType={MealType.Breakfast}
               pageId={1}
@@ -35,7 +37,7 @@ class NoteInputDialogBuilder {
                 isLoading: false,
               }}
               categorySelect={{
-                data: [],
+                data: this._categories,
                 isLoading: false,
               }}
               onSubmit={this._onSubmitMock}
@@ -58,6 +60,11 @@ class NoteInputDialogBuilder {
       defaultQuantity,
     });
 
+    return this;
+  }
+
+  withCategoriesForSelect(...categories: string[]): this {
+    this._categories.push(...categories.map((name, index) => ({ id: index + 1, name })));
     return this;
   }
 
@@ -108,13 +115,105 @@ export const whenProductSelected = async (user: UserEvent, name: RegExp): Promis
   await user.click(screen.getByRole('option', { name }));
 };
 
+export const whenProductCleared = async (user: UserEvent): Promise<void> => {
+  await user.clear(screen.getByRole('combobox', { name: /product/i }));
+};
+
+export const whenAddedNotExistingProductOption = async (
+  user: UserEvent,
+  name: string,
+): Promise<void> => {
+  await user.type(screen.getByRole('combobox', { name: /product/i }), name);
+  await user.click(screen.getByRole('option', { name: new RegExp(`add "${name}"`, 'i') }));
+};
+
+export const whenEditedNotExistingProductOption = async (
+  user: UserEvent,
+  name: string,
+): Promise<void> => {
+  await user.click(screen.getByRole('combobox', { name: /product/i }));
+  await user.click(screen.getByRole('option', { name: new RegExp(`edit "${name}"`, 'i') }));
+};
+
 export const whenQuantityChanged = async (user: UserEvent, value: number): Promise<void> => {
   await user.clear(screen.getByPlaceholderText(/product quantity/i));
   await user.type(screen.getByPlaceholderText(/product quantity/i), `${value}`);
 };
 
-export const whenFormSubmitted = async (user: UserEvent): Promise<void> => {
+export const whenNoteSaved = async (user: UserEvent): Promise<void> => {
   await user.click(screen.getByRole('button', { name: /submit/i }));
+};
+
+export const whenProductNameChanged = async (user: UserEvent, name: string): Promise<void> => {
+  await user.clear(screen.getByPlaceholderText(/product name/i));
+  await user.type(screen.getByPlaceholderText(/product name/i), name);
+};
+
+export const whenProductCaloriesCostChanged = async (
+  user: UserEvent,
+  value: number,
+): Promise<void> => {
+  await user.clear(screen.getByPlaceholderText(/calories cost/i));
+  await user.type(screen.getByPlaceholderText(/calories cost/i), `${value}`);
+};
+
+export const whenProductDefaultQuantityChanged = async (
+  user: UserEvent,
+  value: number,
+): Promise<void> => {
+  await user.clear(screen.getByPlaceholderText(/default quantity/i));
+  await user.type(screen.getByPlaceholderText(/default quantity/i), `${value}`);
+};
+
+export const whenProductCategorySelected = async (user: UserEvent, name: RegExp): Promise<void> => {
+  await user.click(screen.getByRole('combobox', { name: /category/i }));
+  await user.click(screen.getByRole('option', { name }));
+};
+
+export const whenProductAdded = async (user: UserEvent): Promise<void> => {
+  await user.click(screen.getByRole('button', { name: /add product/i }));
+};
+
+export const expectExistingProduct = ({
+  name,
+  defaultQuantity,
+}: Omit<
+  productsModel.AutocompleteExistingOption,
+  'id'
+>): productsModel.AutocompleteExistingOption =>
+  expect.objectContaining<productsModel.AutocompleteExistingOption>({
+    id: expect.any(Number),
+    name,
+    defaultQuantity,
+  });
+
+export const expectNewProduct = ({
+  name,
+  caloriesCost,
+  defaultQuantity,
+  category,
+}: Omit<
+  productsModel.AutocompleteFreeSoloOption,
+  'freeSolo' | 'editing'
+>): productsModel.AutocompleteFreeSoloOption =>
+  expect.objectContaining<productsModel.AutocompleteFreeSoloOption>({
+    freeSolo: true,
+    editing: true,
+    name,
+    caloriesCost,
+    defaultQuantity,
+    category,
+  });
+
+export const expectCategory = (name: string): SelectOption =>
+  expect.objectContaining<Partial<SelectOption>>({ name });
+
+export const thenProductHasValue = async (value: string): Promise<void> => {
+  expect(screen.getByRole('combobox', { name: /product/i })).toHaveValue(value);
+};
+
+export const thenProductIsInvalid = async (): Promise<void> => {
+  expect(screen.getByRole('combobox', { name: /product/i })).toBeInvalid();
 };
 
 export const thenQuantityHasValue = async (value: number): Promise<void> => {
@@ -125,23 +224,51 @@ export const thenDialogShouldBeHidden = async (): Promise<void> => {
   expect(await screen.findByRole('button', { name: /open/i })).toBeVisible();
 };
 
-interface FormSubmittedValue {
-  product: Partial<NoteCreateEdit['product']>;
-  productQuantity: NoteCreateEdit['productQuantity'];
-}
+export const thenProductFormShouldBeVisible = async (): Promise<void> => {
+  expect(screen.getByRole('dialog', { name: /product/i }));
+};
+
+export const thenNoteFormShouldBeVisible = async (): Promise<void> => {
+  expect(await screen.findByText(/note/i));
+};
+
+export const thenProductNameHasValue = async (value: string): Promise<void> => {
+  expect(screen.getByPlaceholderText(/product name/i)).toHaveValue(value);
+};
+
+export const thenProductNameIsInvalid = async (): Promise<void> => {
+  expect(screen.getByPlaceholderText(/product name/i)).toBeInvalid();
+};
+
+export const thenProductCaloriesCostHasValue = async (value: number): Promise<void> => {
+  expect(screen.getByPlaceholderText(/calories cost/i)).toHaveValue(value);
+};
+
+export const thenProductDefaultQuantityHasValue = async (value: number): Promise<void> => {
+  expect(screen.getByPlaceholderText(/default quantity/i)).toHaveValue(value);
+};
+
+export const thenProductCategoryIsEmpty = async (): Promise<void> => {
+  expect(screen.getByRole('combobox', { name: /category/i })).not.toHaveValue();
+};
+
+export const thenSubmitNoteButtonIsDisabled = async (): Promise<void> => {
+  expect(screen.getByRole('button', { name: /submit/i })).toBeDisabled();
+};
+
+export const thenAddProductButtonIsDisabled = async (): Promise<void> => {
+  expect(screen.getByRole('button', { name: /add product/i })).toBeDisabled();
+};
 
 export const thenFormValueContains = async (
   onSubmitMock: Mock,
-  { product, productQuantity }: FormSubmittedValue,
+  { product, productQuantity }: Pick<NoteCreateEdit, 'product' | 'productQuantity'>,
 ): Promise<void> => {
   expect(onSubmitMock).toHaveBeenCalledWith<[NoteCreateEdit]>({
     mealType: MealType.Breakfast,
     pageId: 1,
     displayOrder: 1,
-    product: expect.objectContaining<Partial<productsModel.AutocompleteExistingOption>>({
-      name: product.name,
-      defaultQuantity: product.defaultQuantity,
-    }),
+    product,
     productQuantity,
   });
 };
--- src/frontend/src/features/notes/ui/NoteInputDialog/NoteInputDialog.test.tsx
+++ src/frontend/src/features/notes/ui/NoteInputDialog/NoteInputDialog.test.tsx
@@ -6,10 +6,31 @@ import {
   whenProductSelected,
   thenQuantityHasValue,
   whenQuantityChanged,
-  whenFormSubmitted,
   whenDialogClosed,
   thenFormValueContains,
   thenDialogShouldBeHidden,
+  whenAddedNotExistingProductOption,
+  thenProductFormShouldBeVisible,
+  whenProductNameChanged,
+  whenProductDefaultQuantityChanged,
+  whenProductCaloriesCostChanged,
+  whenProductCategorySelected,
+  whenProductAdded,
+  expectCategory,
+  expectNewProduct,
+  expectExistingProduct,
+  whenNoteSaved,
+  thenProductHasValue,
+  thenNoteFormShouldBeVisible,
+  thenProductNameHasValue,
+  thenProductDefaultQuantityHasValue,
+  thenProductCaloriesCostHasValue,
+  thenProductCategoryIsEmpty,
+  whenProductCleared,
+  thenSubmitNoteButtonIsDisabled,
+  thenProductNameIsInvalid,
+  thenProductIsInvalid,
+  thenAddProductButtonIsDisabled,
 } from './NoteInputDialog.fixture';
 
 describe('when opened for existing note', () => {
@@ -46,12 +67,13 @@ describe('when opened for edit, changed product, closed without save, and opened
     await whenProductSelected(user, /second product/i);
     await whenDialogClosed(user);
     await thenDialogShouldBeHidden();
+
     await whenDialogOpened(user);
     await thenQuantityHasValue(100);
   });
 });
 
-describe('when product changed', () => {
+describe('when changed product', () => {
   test(`should take quantity from product's default quantity if creating note`, async () => {
     const user = userEvent.setup();
 
@@ -84,8 +106,8 @@ describe('when product changed', () => {
   });
 });
 
-describe('when input is valid', () => {
-  test('should submit form', async () => {
+describe('when selected existing product with valid quantity', () => {
+  test('should add new note', async () => {
     const user = userEvent.setup();
     const onSubmitMock = vi.fn();
 
@@ -100,13 +122,181 @@ describe('when input is valid', () => {
     await whenDialogOpened(user);
     await whenProductSelected(user, /test product/i);
     await whenQuantityChanged(user, 150);
-    await whenFormSubmitted(user);
+    await whenNoteSaved(user);
     await thenFormValueContains(onSubmitMock, {
-      product: {
+      product: expectExistingProduct({
         name: 'Test product',
         defaultQuantity: 123,
-      },
+      }),
       productQuantity: 150,
     });
+  });
+});
+
+describe('when added new product with valid quantity', () => {
+  test('should add new note', async () => {
+    const user = userEvent.setup();
+    const onSubmitMock = vi.fn();
+
+    render(
+      givenNoteInputDialog()
+        .withQuantity(100)
+        .withCategoriesForSelect('Test Category')
+        .withOnSubmit(onSubmitMock)
+        .please(),
+    );
+
+    await whenDialogOpened(user);
+    await whenAddedNotExistingProductOption(user, 'New product');
+    await thenProductFormShouldBeVisible();
+
+    await whenProductNameChanged(user, 'New super product');
+    await whenProductCaloriesCostChanged(user, 200);
+    await whenProductDefaultQuantityChanged(user, 175);
+    await whenProductCategorySelected(user, /test category/i);
+    await whenProductAdded(user);
+    await thenQuantityHasValue(175);
+
+    await whenQuantityChanged(user, 150);
+    await whenNoteSaved(user);
+    await thenFormValueContains(onSubmitMock, {
+      product: expectNewProduct({
+        name: 'New super product',
+        defaultQuantity: 175,
+        caloriesCost: 200,
+        category: expectCategory('Test Category'),
+      }),
+      productQuantity: 150,
+    });
+  });
+
+  test('should save existing note', async () => {
+    const user = userEvent.setup();
+    const onSubmitMock = vi.fn();
+
+    render(
+      givenNoteInputDialog()
+        .withQuantity(100)
+        .withProductForSelect({ name: 'Test product', defaultQuantity: 123 })
+        .withCategoriesForSelect('Test Category')
+        .withOnSubmit(onSubmitMock)
+        .withSelectedProduct('Test product')
+        .please(),
+    );
+
+    await whenDialogOpened(user);
+    await whenAddedNotExistingProductOption(user, 'New product');
+    await thenProductFormShouldBeVisible();
+
+    await whenProductNameChanged(user, 'New super product');
+    await whenProductCaloriesCostChanged(user, 200);
+    await whenProductDefaultQuantityChanged(user, 175);
+    await whenProductCategorySelected(user, /test category/i);
+    await whenProductAdded(user);
+    await thenQuantityHasValue(175);
+
+    await whenNoteSaved(user);
+    await thenFormValueContains(onSubmitMock, {
+      product: expectNewProduct({
+        name: 'New super product',
+        defaultQuantity: 175,
+        caloriesCost: 200,
+        category: expectCategory('Test Category'),
+      }),
+      productQuantity: 175,
+    });
+  });
+});
+
+describe('when dialog closed without save', () => {
+  test('should clear note values', async () => {
+    const user = userEvent.setup();
+
+    render(
+      givenNoteInputDialog()
+        .withQuantity(100)
+        .withCategoriesForSelect('Test Category')
+        .withProductForSelect({ name: 'Chicken' })
+        .withProductForSelect({ name: 'Rice' })
+        .withSelectedProduct('Chicken')
+        .please(),
+    );
+
+    await whenDialogOpened(user);
+    await whenProductSelected(user, /rice/i);
+    await whenQuantityChanged(user, 150);
+    await whenDialogClosed(user);
+    await thenDialogShouldBeHidden();
+
+    await whenDialogOpened(user);
+    await thenProductHasValue('Chicken');
+    await thenQuantityHasValue(100);
+  });
+
+  test('should clear product values', async () => {
+    const user = userEvent.setup();
+
+    render(
+      givenNoteInputDialog()
+        .withQuantity(100)
+        .withCategoriesForSelect('Test Category')
+        .withProductForSelect({ name: 'Chicken' })
+        .withProductForSelect({ name: 'Rice' })
+        .withSelectedProduct('Chicken')
+        .please(),
+    );
+
+    await whenDialogOpened(user);
+    await whenAddedNotExistingProductOption(user, 'Bre');
+    await thenProductFormShouldBeVisible();
+
+    await whenProductNameChanged(user, 'Bread');
+    await whenProductCaloriesCostChanged(user, 400);
+    await whenProductDefaultQuantityChanged(user, 50);
+    await whenProductCategorySelected(user, /test category/i);
+    await whenDialogClosed(user);
+    await thenNoteFormShouldBeVisible();
+
+    await whenAddedNotExistingProductOption(user, 'Rye bread');
+    await thenProductFormShouldBeVisible();
+    await thenProductNameHasValue('Rye bread');
+    await thenProductCaloriesCostHasValue(100);
+    await thenProductDefaultQuantityHasValue(100);
+    await thenProductCategoryIsEmpty();
+  });
+});
+
+describe('when input invalid', () => {
+  test('should disable submit button for note', async () => {
+    const user = userEvent.setup();
+
+    render(
+      givenNoteInputDialog()
+        .withQuantity(100)
+        .withCategoriesForSelect('Test Category')
+        .withProductForSelect({ name: 'Chicken' })
+        .withSelectedProduct('Chicken')
+        .please(),
+    );
+
+    await whenDialogOpened(user);
+    await whenProductCleared(user);
+    await thenProductIsInvalid();
+    await thenSubmitNoteButtonIsDisabled();
+  });
+
+  test('should disable submit button for product', async () => {
+    const user = userEvent.setup();
+
+    render(
+      givenNoteInputDialog().withQuantity(100).withCategoriesForSelect('Test Category').please(),
+    );
+
+    await whenDialogOpened(user);
+    await whenAddedNotExistingProductOption(user, 'Test');
+    await whenProductNameChanged(user, 'T');
+    await whenProductCaloriesCostChanged(user, 120);
+    await thenProductNameIsInvalid();
+    await thenAddProductButtonIsDisabled();
   });
 });
