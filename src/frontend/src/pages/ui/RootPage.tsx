05e6e9583bf49d2cb89407ee324dc47a9f5e6d96
Pavel Kirilin <kirilin.pav@gmail.com>
2024-05-09 18:02:13 +0300

    Removed old prompt for update

--- src/frontend/src/pages/ui/RootPage.tsx
+++ src/frontend/src/pages/ui/RootPage.tsx
@@ -1,11 +1,9 @@
 import { type PropsWithChildren, type FC } from 'react';
 import { Outlet, ScrollRestoration } from 'react-router-dom';
-import { ReloadPrompt } from '@/widgets/pwa';
 
 export const RootPage: FC<PropsWithChildren> = ({ children }) => (
   <>
     <ScrollRestoration />
-    <ReloadPrompt />
     <Outlet />
     {children}
   </>
