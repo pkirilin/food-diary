123cb85862b2885266c92b40161456674828a189
Pavel Kirilin <kirilin.pav@gmail.com>
2024-06-09 00:06:18 +0300

    Removed unused code

--- src/frontend/src/shared/lib/fileHelper.ts
+++ src/frontend/src/shared/lib/fileHelper.ts
@@ -32,26 +32,3 @@ export const convertToBase64String = (file: File): Promise<string> =>
 
     reader.readAsDataURL(file);
   });
-
-// export function compressImage(base64Image: string, size: number): Promise<string> {
-//   const canvas = document.createElement('canvas');
-//   const ctx = canvas.getContext('2d');
-//   const image = new Image();
-//   image.src = base64Image;
-
-//   return new Promise(resolve => {
-//     image.onload = () => {
-//       const width = Math.min(size, image.width);
-//       const height = (image.height / image.width) * width;
-
-//       canvas.width = width;
-//       canvas.height = height;
-
-//       ctx?.drawImage(image, 0, 0, canvas.width, canvas.height);
-
-//       const compressedBase64Image = canvas.toDataURL(`image/jpeg`, 1);
-
-//       return resolve(compressedBase64Image);
-//     };
-//   });
-// }
