0efec5b90841c15cb0c2934cacb3d2b228d6f6e0
Pavel Kirilin <kirilin.pav@gmail.com>
2024-08-27 23:45:46 +0300

    Added history filter UI

--- src/frontend/src/pages/ui/HistoryPage.tsx
+++ src/frontend/src/pages/ui/HistoryPage.tsx
@@ -1,28 +1,38 @@
 import AddIcon from '@mui/icons-material/Add';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import FilterAltOffIcon from '@mui/icons-material/FilterAltOff';
 import {
+  Box,
+  Collapse,
   IconButton,
   List,
   ListItem,
   ListItemButton,
+  ListItemIcon,
   ListItemSecondaryAction,
   ListItemText,
   Paper,
   Stack,
+  TextField,
   Tooltip,
   Typography,
 } from '@mui/material';
+import { DatePicker } from '@mui/x-date-pickers';
 import { type FC } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { store } from '@/app/store';
 import { type NoteHistoryItem, noteApi } from '@/entities/note';
 import { MSW_ENABLED } from '@/shared/config';
+import { useToggle } from '@/shared/hooks';
 import { dateLib } from '@/shared/lib';
 import { PrivateLayout } from '@/widgets/layout';
 import { withAuthStatusCheck } from '../lib';
 
 interface LoaderData {
   notes: NoteHistoryItem[];
+  today: Date;
 }
 
 export const loader = withAuthStatusCheck(async () => {
@@ -37,11 +47,13 @@ export const loader = withAuthStatusCheck(async () => {
 
   return {
     notes: notesHistoryQuery.data?.notesHistory ?? [],
+    today,
   } satisfies LoaderData;
 });
 
 export const Component: FC = () => {
-  const { notes } = useLoaderData() as LoaderData;
+  const { notes, today } = useLoaderData() as LoaderData;
+  const [filterVisible, toggleFilter] = useToggle();
 
   return (
     <PrivateLayout
@@ -51,31 +63,49 @@ export const Component: FC = () => {
           direction="row"
           justifyContent="space-between"
           alignItems="center"
-          gap={2}
+          spacing={1}
         >
           <Typography variant="h6" component="h1">
             History
           </Typography>
-          <Tooltip title="Add notes">
-            <IconButton edge="end">
-              <AddIcon />
-            </IconButton>
-          </Tooltip>
+
+          <Box display="flex" gap={1}>
+            <Tooltip title="Add notes">
+              <IconButton>
+                <AddIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title={filterVisible ? 'Hide filter' : 'Show filter'}>
+              <IconButton edge="end" onClick={toggleFilter}>
+                {filterVisible ? <FilterAltOffIcon /> : <FilterAltIcon />}
+              </IconButton>
+            </Tooltip>
+          </Box>
         </Stack>
       }
     >
-      <Stack mt={2} component={Paper}>
+      <Collapse in={filterVisible}>
+        <DatePicker
+          views={['year', 'month']}
+          label="Year and Month"
+          value={today}
+          onChange={_ => {}}
+          renderInput={params => <TextField {...params} size="small" margin="normal" />}
+        />
+      </Collapse>
+      <Stack mt={2} spacing={2} component={Paper}>
         <List disablePadding>
           {notes.map(({ date, caloriesCount }) => (
-            <ListItem key={date} disablePadding>
+            <ListItem key={date} disableGutters>
               <ListItemButton>
-                <ListItemText
-                  primary={dateLib.formatToUserFriendlyString(date)}
-                  secondary={`${caloriesCount} kcal`}
-                />
-                <ListItemSecondaryAction>
+                <ListItemIcon>
+                  <CalendarTodayIcon />
+                </ListItemIcon>
+                <ListItemText primary={dateLib.formatToUserFriendlyString(date)} />
+                <Box component={ListItemSecondaryAction} display="flex" alignItems="center" gap={1}>
+                  <ListItemText secondary={`${caloriesCount} kcal`} />
                   <ChevronRightIcon />
-                </ListItemSecondaryAction>
+                </Box>
               </ListItemButton>
             </ListItem>
           ))}
