a27628b1ce5498920c1006908a0b0aa1ca194676
Pavel Kirilin <kirilin.pav@gmail.com>
2024-09-05 21:24:46 +0300

    Fix create/update note after date change (#118)
    * Updated react router
    
    * Force update note form values after rrd updates loader data
    
    * Removed unused args from note tags

--- src/frontend/src/entities/note/lib/useFormValues.ts
+++ src/frontend/src/entities/note/lib/useFormValues.ts
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { type FormValues } from '../model';
 
 interface Result {
@@ -9,6 +9,14 @@ interface Result {
 
 export const useFormValues = (initialValues: FormValues): Result => {
   const [values, setValues] = useState<FormValues>(initialValues);
+
+  // Used to catch latest date updates from react-router-dom loader data
+  useEffect(() => {
+    setValues(prev => ({
+      ...prev,
+      date: initialValues.date,
+    }));
+  }, [initialValues.date]);
 
   const clearValues = useCallback(() => {
     setValues(initialValues);
