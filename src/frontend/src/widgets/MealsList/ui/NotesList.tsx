1e2740fd011ccc89d3d231600cba50e9d91cc411
Pavel Kirilin <kirilin.pav@gmail.com>
2024-09-12 22:12:57 +0300

    Fix display order calculation

--- src/frontend/src/widgets/MealsList/ui/NotesList.tsx
+++ src/frontend/src/widgets/MealsList/ui/NotesList.tsx
@@ -1,6 +1,6 @@
 import { List, ListItem } from '@mui/material';
 import { type FC } from 'react';
-import { type NoteItem, type noteModel } from '@/entities/note';
+import { noteLib, type NoteItem, type noteModel } from '@/entities/note';
 import { AddNote } from '@/features/note/addEdit';
 import { NotesListItem } from './NotesListItem';
 
@@ -10,13 +10,17 @@ interface Props {
   notes: NoteItem[];
 }
 
-export const NotesList: FC<Props> = ({ date, mealType, notes }) => (
-  <List disablePadding>
-    {notes.map(note => (
-      <NotesListItem key={note.id} note={note} />
-    ))}
-    <ListItem disableGutters>
-      <AddNote date={date} mealType={mealType} />
-    </ListItem>
-  </List>
-);
+export const NotesList: FC<Props> = ({ date, mealType, notes }) => {
+  const displayOrder = noteLib.useNextDisplayOrder(notes);
+
+  return (
+    <List disablePadding>
+      {notes.map(note => (
+        <NotesListItem key={note.id} note={note} />
+      ))}
+      <ListItem disableGutters>
+        <AddNote date={date} mealType={mealType} displayOrder={displayOrder} />
+      </ListItem>
+    </List>
+  );
+};
