cc0eb2534a1463b27b6b6a820155495d0e8eb5d5
Pavel Kirilin <kirilin.pav@gmail.com>
2024-09-19 19:31:10 +0300

    Fixed loaders activation before msw started

--- src/frontend/src/app/Root/Root.tsx
+++
@@ -1,6 +0,0 @@
-import { type FC } from 'react';
-import { RouterProvider } from 'react-router-dom';
-import { AppLoader } from '@/shared/ui';
-import { router } from '../routing';
-
-export const Root: FC = () => <RouterProvider router={router} fallbackElement={<AppLoader />} />;
