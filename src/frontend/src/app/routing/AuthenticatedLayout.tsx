62501b783e375e9c067f99bcaa929138831846a6
Pavel Kirilin <kirilin.pav@gmail.com>
2024-09-19 19:51:15 +0300

    Add lazy loading for AuthenticatedLayout

--- src/frontend/src/app/routing/AuthenticatedLayout.tsx
+++ src/frontend/src/app/routing/AuthenticatedLayout.tsx
@@ -1,11 +1,43 @@
 import { AppBar, Container, LinearProgress, Toolbar } from '@mui/material';
 import { type FC } from 'react';
-import { Outlet, ScrollRestoration } from 'react-router-dom';
-import { useAuthStatusCheckEffect } from '@/features/auth';
+import {
+  type LoaderFunction,
+  Outlet,
+  ScrollRestoration,
+  redirect,
+  type ShouldRevalidateFunction,
+} from 'react-router-dom';
+import { authApi, useAuthStatusCheckEffect } from '@/features/auth';
+import { ok } from '@/pages/lib';
 import { APP_BAR_HEIGHT_SM, APP_BAR_HEIGHT_XS } from '@/shared/constants';
+import { createUrl } from '@/shared/lib';
 import { Navigation, useNavigationProgress } from '@/widgets/Navigation';
+import { store } from '../store';
+import { Error } from './Error';
 
-export const AuthenticatedLayout: FC = () => {
+export const loader: LoaderFunction = async ({ request }) => {
+  const authStatusQuery = await store.dispatch(
+    authApi.endpoints.getStatus.initiate({}, { forceRefetch: true }),
+  );
+
+  if (!authStatusQuery.data?.isAuthenticated) {
+    const returnUrl = new URL(request.url).searchParams.get('returnUrl') ?? '/';
+    const loginUrl = createUrl('/login', { returnUrl });
+    return redirect(loginUrl);
+  }
+
+  return ok();
+};
+
+export const shouldRevalidate: ShouldRevalidateFunction = () => true;
+
+export const ErrorBoundary: FC = () => (
+  <Container sx={{ py: 2 }}>
+    <Error />
+  </Container>
+);
+
+export const Component: FC = () => {
   const progress = useNavigationProgress();
 
   useAuthStatusCheckEffect();
--- src/frontend/src/app/routing/createRouter.tsx
+++ src/frontend/src/app/routing/createRouter.tsx
@@ -1,38 +1,12 @@
 import { Container } from '@mui/material';
-import { type LoaderFunction, createBrowserRouter, redirect } from 'react-router-dom';
-import { authApi } from '@/features/auth';
-import { ok } from '@/pages/lib';
-import { createUrl } from '@/shared/lib';
-import { store } from '../store';
-import { AuthenticatedLayout } from './AuthenticatedLayout';
+import { createBrowserRouter } from 'react-router-dom';
 import { Error } from './Error';
 import { UnauthenticatedLayout } from './UnauthenticatedLayout';
-
-const authLoader: LoaderFunction = async ({ request }) => {
-  const authStatusQuery = await store.dispatch(
-    authApi.endpoints.getStatus.initiate({}, { forceRefetch: true }),
-  );
-
-  if (!authStatusQuery.data?.isAuthenticated) {
-    const returnUrl = new URL(request.url).searchParams.get('returnUrl') ?? '/';
-    const loginUrl = createUrl('/login', { returnUrl });
-    return redirect(loginUrl);
-  }
-
-  return ok();
-};
 
 export const createRouter = (): ReturnType<typeof createBrowserRouter> =>
   createBrowserRouter([
     {
-      loader: authLoader,
-      shouldRevalidate: () => true,
-      element: <AuthenticatedLayout />,
-      errorElement: (
-        <Container sx={{ py: 2 }}>
-          <Error />
-        </Container>
-      ),
+      lazy: () => import('./AuthenticatedLayout'),
       children: [
         {
           errorElement: <Error />,
