df72c2e1fc5cbcbce965a5c11bc36587f15dc1d4
Pavel Kirilin <kirilin.pav@gmail.com>
2024-09-21 19:32:11 +0300

    Add UI for logging weight

--- src/frontend/src/pages/ui/WeightPage.tsx
+++ src/frontend/src/pages/ui/WeightPage.tsx
@@ -1,7 +1,10 @@
+import { List, ListItem, ListItemText, ListSubheader } from '@mui/material';
 import { type FC } from 'react';
 import { type LoaderFunction } from 'react-router-dom';
 import { store } from '@/app/store';
 import { weightLogsApi } from '@/entities/weightLog';
+import { AddWeightLog } from '@/features/logWeight';
+import { dateLib } from '@/shared/lib';
 import { type NavigationLoaderData } from '@/widgets/Navigation';
 import { WeightChart } from '@/widgets/WeightChart';
 
@@ -22,4 +25,36 @@ export const loader: LoaderFunction = async () => {
   }
 };
 
-export const Component: FC = () => <WeightChart />;
+export const Component: FC = () => {
+  const { weightLogs } = weightLogsApi.useWeightLogsQuery(null, {
+    selectFromResult: ({ data }) => ({
+      weightLogs: data?.weightLogs ?? [],
+    }),
+  });
+
+  const lastLog = weightLogs.at(-1);
+
+  return (
+    <>
+      <WeightChart />
+      {lastLog && (
+        <List>
+          <ListSubheader
+            disableGutters
+            sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
+          >
+            <span>Last logged</span>
+            <AddWeightLog />
+          </ListSubheader>
+
+          <ListItem disableGutters disablePadding>
+            <ListItemText
+              primary={`${lastLog.value} kg`}
+              secondary={dateLib.formatToUserFriendlyString(lastLog.date)}
+            />
+          </ListItem>
+        </List>
+      )}
+    </>
+  );
+};
