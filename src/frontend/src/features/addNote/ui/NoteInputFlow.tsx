ddcb2c43dacd9cd3e2592104bc6e3adb4f51f2f5
Pavel Kirilin <kirilin.pav@gmail.com>
2024-10-23 23:09:12 +0300

    Use first category as a default value

--- src/frontend/src/features/addNote/ui/NoteInputFlow.tsx
+++ src/frontend/src/features/addNote/ui/NoteInputFlow.tsx
@@ -2,28 +2,26 @@ import { type FC } from 'react';
 import { useAppDispatch, useAppSelector } from '@/app/store';
 import { categoryLib } from '@/entities/category';
 import { type productModel } from '@/entities/product';
+import { type SelectOption } from '@/shared/types';
 import { actions } from '../model';
 import { type ProductFormValues } from '../model/productForm';
 import { ProductForm } from './ProductForm';
 import { QuantityForm } from './QuantityForm';
 import { SearchProducts } from './SearchProducts';
 
-const toProductFormValues = ({
+const toProductFormValues = (
+  { name, caloriesCost, defaultQuantity, category }: productModel.AutocompleteFreeSoloOption,
+  categories: SelectOption[],
+): ProductFormValues => ({
   name,
   caloriesCost,
   defaultQuantity,
-  category,
-}: productModel.AutocompleteFreeSoloOption): ProductFormValues => ({
-  name,
-  caloriesCost,
-  defaultQuantity,
-  // TODO: use first category as a default value
-  category,
+  category: categories.at(0) ?? category,
 });
 
 export const NoteInputFlow: FC = () => {
   const product = useAppSelector(state => state.addNote.draft?.product);
-  const categorySelectData = categoryLib.useCategorySelectData();
+  const categorySelect = categoryLib.useCategorySelectData();
   const dispatch = useAppDispatch();
 
   if (!product) {
@@ -33,9 +31,9 @@ export const NoteInputFlow: FC = () => {
   if (product.freeSolo && product.editing) {
     return (
       <ProductForm
-        defaultValues={toProductFormValues(product)}
-        categories={categorySelectData.data}
-        categoriesLoading={categorySelectData.isLoading}
+        defaultValues={toProductFormValues(product, categorySelect.data)}
+        categories={categorySelect.data}
+        categoriesLoading={categorySelect.isLoading}
         onSubmit={data => dispatch(actions.productSaved(data))}
       />
     );
