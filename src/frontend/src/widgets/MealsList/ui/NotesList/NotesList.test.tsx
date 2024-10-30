f40cd7f4c74f6cb831f4ce1af17be8f07f4493f5
Pavel Kirilin <kirilin.pav@gmail.com>
2024-10-30 22:03:23 +0300

    Add test for add note with adding product "on the fly"

--- src/frontend/src/widgets/MealsList/ui/NotesList/NotesList.test.tsx
+++ src/frontend/src/widgets/MealsList/ui/NotesList/NotesList.test.tsx
@@ -32,3 +32,41 @@ test('I can add new note with existing product', async () => {
 
   expect(screen.getByRole('button', { name: /cheese 120 g 482 kcal/i })).toBeVisible();
 });
+
+test('I can add new note with adding new product "on the fly"', async () => {
+  const user = userEvent.setup();
+  const store = configureStore();
+  await store.dispatch(noteApi.endpoints.notes.initiate({ date: '2023-10-19' }));
+
+  render(
+    <RootProvider store={store}>
+      <NotesList date="2023-10-19" mealType={MealType.Lunch} />
+    </RootProvider>,
+  );
+
+  await user.click(screen.getByRole('button', { name: /add note/i }));
+  expect(await screen.findByRole('dialog', { name: /new note/i })).toBeVisible();
+
+  await user.type(screen.getByPlaceholderText(/search products/i), 'Ora');
+  await user.click(await screen.findByRole('button', { name: /add "ora"/i }));
+  expect(screen.getByRole('dialog', { name: /new product/i })).toBeVisible();
+
+  await user.type(screen.getByPlaceholderText(/name/i), 'nge');
+  await user.clear(screen.getByPlaceholderText(/calories cost/i));
+  await user.type(screen.getByPlaceholderText(/calories cost/i), '60');
+  await user.clear(screen.getByPlaceholderText(/default quantity/i));
+  await user.type(screen.getByPlaceholderText(/default quantity/i), '200');
+  await user.click(screen.getByRole('combobox', { name: /category/i }));
+  await user.click(screen.getByRole('option', { name: /fruits/i }));
+  await user.click(screen.getByRole('button', { name: /add/i }));
+  expect(await screen.findByRole('dialog', { name: /new note/i })).toBeVisible();
+  expect(screen.getByRole('textbox', { name: /product/i })).toHaveValue('Orange');
+  expect(screen.getByPlaceholderText(/quantity/i)).toHaveValue('200');
+
+  await user.clear(screen.getByPlaceholderText(/quantity/i));
+  await user.type(screen.getByPlaceholderText(/quantity/i), '250');
+  await user.click(screen.getByRole('button', { name: /add/i }));
+  await waitForElementToBeRemoved(screen.getByRole('dialog'));
+
+  expect(screen.getByRole('button', { name: /orange 250 g 150 kcal/i })).toBeVisible();
+});
