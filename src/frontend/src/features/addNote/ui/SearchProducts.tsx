e11c7a425bd719346cd1fd79ead9cbf70d9a7703
Pavel Kirilin <kirilin.pav@gmail.com>
2025-02-02 17:04:57 +0300

    Use event instead of effect for debounced search

--- src/frontend/src/features/addNote/ui/SearchProducts.tsx
+++ src/frontend/src/features/addNote/ui/SearchProducts.tsx
@@ -1,51 +1,51 @@
 import ClearIcon from '@mui/icons-material/Clear';
 import SearchIcon from '@mui/icons-material/Search';
 import { IconButton, InputAdornment, TextField } from '@mui/material';
-import { useState, type FC, useEffect } from 'react';
-import { useForm } from 'react-hook-form';
-import { useDebounce } from 'use-debounce';
+import { useState, type FC, type ChangeEventHandler } from 'react';
+import { useDebounce, useDebouncedCallback } from 'use-debounce';
 import { type ProductSelectOption, productApi } from '@/entities/product';
 import { searchProductsByName } from '../lib/searchProductsByName';
 import { FoundProductsList } from './FoundProductsList';
 import { UploadImageButton } from './UploadImageButton';
 
-interface FormValues {
-  query: string;
-}
-
 const DEBOUNCE_QUERY_DELAY = 300;
 const EMPTY_PRODUCTS: ProductSelectOption[] = [];
 
 export const SearchProducts: FC = () => {
-  const { register, watch, reset } = useForm<FormValues>({
-    mode: 'onChange',
-    defaultValues: {
-      query: '',
-    },
-  });
+  const [query, setQuery] = useState('');
+  const [foundProducts, setFoundProducts] = useState<ProductSelectOption[]>([]);
 
   const { products } = productApi.useProductsAutocompleteQuery(null, {
     selectFromResult: ({ data }) => ({ products: data ?? EMPTY_PRODUCTS }),
   });
 
-  const query = watch('query');
-  const [debouncedQuery] = useDebounce(query, DEBOUNCE_QUERY_DELAY);
-  const [foundProducts, setFoundProducts] = useState<ProductSelectOption[]>([]);
+  const search = useDebouncedCallback((input: string) => {
+    setFoundProducts(searchProductsByName(products, input));
+  }, DEBOUNCE_QUERY_DELAY);
 
-  useEffect(() => {
-    setFoundProducts(searchProductsByName(products, debouncedQuery));
-  }, [products, debouncedQuery]);
+  const [debouncedQuery] = useDebounce(query, DEBOUNCE_QUERY_DELAY);
+
+  const handleQueryChange: ChangeEventHandler<HTMLInputElement> = event => {
+    setQuery(event.target.value);
+    search(event.target.value);
+  };
+
+  const handleClear = (): void => {
+    setQuery('');
+    setFoundProducts([]);
+  };
 
   return (
     <>
       <TextField
-        {...register('query')}
         fullWidth
         autoFocus
         variant="outlined"
         role="search"
         placeholder="Search products"
         margin="dense"
+        value={query}
+        onChange={handleQueryChange}
         slotProps={{
           input: {
             startAdornment: (
@@ -56,7 +56,7 @@ export const SearchProducts: FC = () => {
             endAdornment: (
               <InputAdornment position="end">
                 {query && (
-                  <IconButton onClick={() => reset()}>
+                  <IconButton onClick={handleClear}>
                     <ClearIcon />
                   </IconButton>
                 )}
