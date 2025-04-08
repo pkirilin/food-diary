70697298ef39499f95c1550ff6208e35e271f81f
Pavel Kirilin <kirilin.pav@gmail.com>
2025-04-09 00:08:29 +0300

    Fix products search debounce bug

--- src/frontend/src/features/manageNote/ui/SearchProducts.tsx
+++ src/frontend/src/features/manageNote/ui/SearchProducts.tsx
@@ -1,8 +1,8 @@
 import ClearIcon from '@mui/icons-material/Clear';
 import SearchIcon from '@mui/icons-material/Search';
 import { IconButton, InputAdornment, TextField } from '@mui/material';
-import { useState, type FC, type ChangeEventHandler } from 'react';
-import { useDebounce, useDebouncedCallback } from 'use-debounce';
+import { useState, type FC, type ChangeEventHandler, useMemo } from 'react';
+import { useDebounce } from 'use-debounce';
 import { type ProductSelectOption, productApi } from '@/entities/product';
 import { searchProductsByName } from '../lib/searchProductsByName';
 import { FoundProductsList } from './FoundProductsList';
@@ -13,26 +13,23 @@ const EMPTY_PRODUCTS: ProductSelectOption[] = [];
 
 export const SearchProducts: FC = () => {
   const [query, setQuery] = useState('');
-  const [foundProducts, setFoundProducts] = useState<ProductSelectOption[]>([]);
+  const [debouncedQuery] = useDebounce(query, DEBOUNCE_QUERY_DELAY);
 
   const { products } = productApi.useProductsAutocompleteQuery(null, {
     selectFromResult: ({ data }) => ({ products: data ?? EMPTY_PRODUCTS }),
   });
 
-  const search = useDebouncedCallback((input: string) => {
-    setFoundProducts(searchProductsByName(products, input));
-  }, DEBOUNCE_QUERY_DELAY);
-
-  const [debouncedQuery] = useDebounce(query, DEBOUNCE_QUERY_DELAY);
+  const foundProducts = useMemo<ProductSelectOption[]>(
+    () => searchProductsByName(products, debouncedQuery),
+    [products, debouncedQuery],
+  );
 
   const handleQueryChange: ChangeEventHandler<HTMLInputElement> = event => {
     setQuery(event.target.value);
-    search(event.target.value);
   };
 
   const handleClear = (): void => {
     setQuery('');
-    setFoundProducts([]);
   };
 
   return (
