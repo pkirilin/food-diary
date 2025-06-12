a2e47d020fa37a70189382d59424079f76dd21ec
Pavel Kirilin <kirilin.pav@gmail.com>
2025-06-12 13:52:58 +0300

    Move NotesList tests to MealsListItem tests

--- src/frontend/src/widgets/MealsList/ui/MealsListItem.steps.tsx
+++ src/frontend/src/widgets/MealsList/ui/MealsListItem.steps.tsx
@@ -1,4 +1,5 @@
-import { render } from '@testing-library/react';
+import { render, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
+import { type UserEvent } from '@testing-library/user-event';
 import { RootProvider } from '@/app/RootProvider';
 import { configureStore } from '@/app/store';
 import { type noteModel } from '@/entities/note';
@@ -6,7 +7,6 @@ import { MealsListItem } from './MealsListItem';
 
 interface GivenMealsListItemArgs {
   mealType: noteModel.MealType;
-  preloadNotes?: boolean;
 }
 
 export const givenMealsListItem = async ({ mealType }: GivenMealsListItemArgs): Promise<void> => {
@@ -18,4 +18,131 @@ export const givenMealsListItem = async ({ mealType }: GivenMealsListItemArgs): 
       <MealsListItem date={date} mealType={mealType} />
     </RootProvider>,
   );
+};
+
+export const whenAddNoteButtonClicked = async (user: UserEvent): Promise<void> => {
+  const addNoteButton = screen.getByRole('button', { name: /add note/i });
+  await waitFor(() => expect(addNoteButton).not.toBeDisabled());
+  await user.click(addNoteButton);
+};
+
+export const whenNoteExpanded = async (user: UserEvent, noteName: RegExp): Promise<void> => {
+  const noteButton = await screen.findByRole('button', { name: noteName });
+  await user.click(noteButton);
+};
+
+export const whenEditNoteClicked = async (user: UserEvent): Promise<void> => {
+  const noteButton = await screen.findByRole('button', { name: /edit/i });
+  await user.click(noteButton);
+};
+
+export const whenProductSearched = async (user: UserEvent, searchText: string): Promise<void> => {
+  await user.type(screen.getByPlaceholderText(/search products/i), searchText);
+};
+
+export const whenExistingProductSelected = async (
+  user: UserEvent,
+  productName: RegExp,
+): Promise<void> => {
+  await user.click(await screen.findByRole('button', { name: productName }));
+};
+
+export const whenQuantityChanged = async (user: UserEvent, quantity: number): Promise<void> => {
+  await user.clear(screen.getByPlaceholderText(/quantity/i));
+  await user.type(screen.getByPlaceholderText(/quantity/i), quantity.toString());
+};
+
+export const whenProductAddedFromInput = async (
+  user: UserEvent,
+  searchText: string,
+): Promise<void> => {
+  await user.click(
+    await screen.findByRole('button', { name: new RegExp(`add "${searchText}"`, 'i') }),
+  );
+};
+
+export const whenProductNameCompleted = async (user: UserEvent, nameEnd: string): Promise<void> => {
+  await user.type(screen.getByPlaceholderText(/name/i), nameEnd);
+};
+
+export const whenProductNameEdited = async (user: UserEvent, name: string): Promise<void> => {
+  await user.clear(screen.getByPlaceholderText(/name/i));
+  await user.type(screen.getByPlaceholderText(/name/i), name);
+};
+
+export const whenProductCaloriesSet = async (user: UserEvent, calories: number): Promise<void> => {
+  await user.clear(screen.getByPlaceholderText(/calories/i));
+  await user.type(screen.getByPlaceholderText(/calories/i), calories.toString());
+};
+
+export const whenProductDefaultQuantitySet = async (
+  user: UserEvent,
+  quantity: number,
+): Promise<void> => {
+  await user.clear(screen.getByPlaceholderText(/default quantity/i));
+  await user.type(screen.getByPlaceholderText(/default quantity/i), quantity.toString());
+};
+
+export const whenProductCategorySelected = async (
+  user: UserEvent,
+  category: RegExp,
+): Promise<void> => {
+  await user.click(screen.getByRole('combobox', { name: /category/i }));
+  await user.click(screen.getByRole('option', { name: category }));
+};
+
+export const whenProductAdded = async (user: UserEvent): Promise<void> => {
+  await user.click(screen.getByRole('button', { name: /add/i }));
+};
+
+export const whenProductSaved = async (user: UserEvent): Promise<void> => {
+  await user.click(screen.getByRole('button', { name: /save/i }));
+};
+
+export const whenNoteAdded = async (user: UserEvent): Promise<void> => {
+  await user.click(screen.getByRole('button', { name: /add/i }));
+};
+
+export const whenNoteSaved = async (user: UserEvent): Promise<void> => {
+  await user.click(screen.getByRole('button', { name: /save/i }));
+};
+
+export const whenProductEditClicked = async (user: UserEvent): Promise<void> => {
+  await user.click(screen.getByRole('button', { name: /edit product/i }));
+};
+
+export const thenDialogVisible = async (dialogTitle: RegExp): Promise<void> => {
+  expect(await screen.findByRole('dialog', { name: dialogTitle })).toBeVisible();
+};
+
+export const thenNoteCannotBeAdded = async (): Promise<void> => {
+  expect(screen.getByRole('button', { name: /add/i })).toBeDisabled();
+};
+
+export const thenNoteCanBeAdded = async (): Promise<void> => {
+  expect(screen.getByRole('button', { name: /add/i })).not.toBeDisabled();
+};
+
+export const thenProductCanBeAdded = async (): Promise<void> => {
+  expect(screen.getByRole('button', { name: /add/i })).not.toBeDisabled();
+};
+
+export const thenProductCanBeSaved = async (): Promise<void> => {
+  expect(screen.getByRole('button', { name: /save/i })).not.toBeDisabled();
+};
+
+export const thenDialogNotVisible = async (): Promise<void> => {
+  await waitForElementToBeRemoved(screen.getByRole('dialog'));
+};
+
+export const thenProductHasValue = async (expectedValue: string): Promise<void> => {
+  expect(screen.getByRole('textbox', { name: /product/i })).toHaveValue(expectedValue);
+};
+
+export const thenQuantityHasValue = (expectedValue: string): void => {
+  expect(screen.getByPlaceholderText(/quantity/i)).toHaveValue(expectedValue);
+};
+
+export const thenSingleNoteVisible = async (noteName: RegExp): Promise<void> => {
+  expect(screen.getByRole('button', { name: noteName })).toBeVisible();
 };
--- src/frontend/src/widgets/MealsList/ui/MealsListItem.test.tsx
+++ src/frontend/src/widgets/MealsList/ui/MealsListItem.test.tsx
@@ -1,15 +1,98 @@
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { noteModel } from '@/entities/note';
 import * as steps from './MealsListItem.steps';
 
 test('I can see my meals with calculated calories', async () => {
-  await steps.givenMealsListItem({
-    mealType: noteModel.MealType.Lunch,
-    preloadNotes: true,
-  });
+  await steps.givenMealsListItem({ mealType: noteModel.MealType.Lunch });
 
   expect(
     await screen.findByRole('listitem', { name: /lunch, [1-9][0-9]* kilocalories/i }),
   ).toBeVisible();
   expect(screen.getAllByRole('button').length).toBeGreaterThan(0);
+});
+
+test('I can add new note with existing product', async () => {
+  const user = userEvent.setup();
+
+  await steps.givenMealsListItem({ mealType: noteModel.MealType.Lunch });
+
+  await steps.whenAddNoteButtonClicked(user);
+  await steps.thenDialogVisible(/lunch/i);
+  await steps.thenNoteCannotBeAdded();
+
+  await steps.whenProductSearched(user, 'che');
+  await steps.whenExistingProductSelected(user, /cheese/i);
+  await steps.thenProductHasValue('Cheese');
+  await steps.thenNoteCanBeAdded();
+
+  await steps.whenQuantityChanged(user, 120);
+  await steps.whenNoteAdded(user);
+  await steps.thenDialogNotVisible();
+  await steps.thenSingleNoteVisible(/cheese 120 g 482 kcal/i);
+});
+
+test('I can add new note with adding new product "on the fly"', async () => {
+  const user = userEvent.setup();
+
+  await steps.givenMealsListItem({ mealType: noteModel.MealType.Lunch });
+
+  await steps.whenAddNoteButtonClicked(user);
+  await steps.thenDialogVisible(/lunch/i);
+
+  await steps.whenProductSearched(user, 'Ora');
+  await steps.whenProductAddedFromInput(user, 'Ora');
+  await steps.thenDialogVisible(/product/i);
+  await steps.thenProductCanBeAdded();
+
+  await steps.whenProductNameCompleted(user, 'nge');
+  await steps.whenProductCaloriesSet(user, 60);
+  await steps.whenProductDefaultQuantitySet(user, 200);
+  await steps.whenProductCategorySelected(user, /fruits/i);
+  await steps.whenProductAdded(user);
+  await steps.thenDialogVisible(/lunch/i);
+  await steps.thenProductHasValue('Orange');
+  steps.thenQuantityHasValue('200');
+
+  await steps.whenQuantityChanged(user, 250);
+  await steps.whenNoteAdded(user);
+  await steps.thenDialogNotVisible();
+  await steps.thenSingleNoteVisible(/orange 250 g 150 kcal/i);
+});
+
+test('I can change quantity for existing note', async () => {
+  const user = userEvent.setup();
+
+  await steps.givenMealsListItem({ mealType: noteModel.MealType.Lunch });
+
+  await steps.whenNoteExpanded(user, /cheese 200 g 804 kcal/i);
+  await steps.whenEditNoteClicked(user);
+  await steps.thenDialogVisible(/lunch/i);
+  await steps.thenProductHasValue('Cheese');
+  steps.thenQuantityHasValue('200');
+
+  await steps.whenQuantityChanged(user, 150);
+  await steps.whenNoteSaved(user);
+  await steps.thenDialogNotVisible();
+  await steps.thenSingleNoteVisible(/cheese 150 g 603 kcal/i);
+});
+
+test('I can edit product via note form', async () => {
+  const user = userEvent.setup();
+
+  await steps.givenMealsListItem({ mealType: noteModel.MealType.Lunch });
+
+  await steps.whenAddNoteButtonClicked(user);
+  await steps.thenDialogVisible(/lunch/i);
+
+  await steps.whenProductSearched(user, 'che');
+  await steps.whenExistingProductSelected(user, /cheese/i);
+  await steps.whenProductEditClicked(user);
+  await steps.thenDialogVisible(/product/i);
+  await steps.thenProductCanBeSaved();
+
+  await steps.whenProductNameEdited(user, 'Mozarella cheese');
+  await steps.whenProductSaved(user);
+  await steps.thenDialogVisible(/lunch/i);
+  await steps.thenProductHasValue('Mozarella cheese');
 });
