617850464abaa50d6d4dee7450dab0f905db5b2f
Pavel Kirilin <kirilin.pav@gmail.com>
2025-06-22 21:31:58 +0300

    Center nutrition summary grid items

--- src/frontend/src/widgets/NutritionSummaryWidget/NutritionSummaryWidget.tsx
+++ src/frontend/src/widgets/NutritionSummaryWidget/NutritionSummaryWidget.tsx
@@ -11,24 +11,24 @@ export const NutritionSummaryWidget: FC<Props> = ({ nutritionValues }) => {
   const { calories, protein, fats, carbs, sugar, salt } = nutritionValues;
 
   return (
-    <Box px={1} py={2} component={Container}>
+    <Box pt={3} pb={2} component={Container}>
       <Grid container spacing={2}>
-        <Grid size={4}>
+        <Grid size={4} display="flex" justifyContent="center">
           <NutritionSummaryItem type="calories" value={calories} />
         </Grid>
-        <Grid size={4}>
+        <Grid size={4} display="flex" justifyContent="center">
           <NutritionSummaryItem type="protein" value={protein} />
         </Grid>
-        <Grid size={4}>
+        <Grid size={4} display="flex" justifyContent="center">
           <NutritionSummaryItem type="fats" value={fats} />
         </Grid>
-        <Grid size={4}>
+        <Grid size={4} display="flex" justifyContent="center">
           <NutritionSummaryItem type="carbs" value={carbs} />
         </Grid>
-        <Grid size={4}>
+        <Grid size={4} display="flex" justifyContent="center">
           <NutritionSummaryItem type="sugar" value={sugar} />
         </Grid>
-        <Grid size={4}>
+        <Grid size={4} display="flex" justifyContent="center">
           <NutritionSummaryItem type="salt" value={salt} />
         </Grid>
       </Grid>
