ddb45f1ed85424c83bf0fb90bf92b7e4d3cfabae
Pavel Kirilin <kirilin.pav@gmail.com>
2025-06-23 21:37:15 +0300

    Nutrition summary: use paper with vertical layout

--- src/frontend/src/widgets/NutritionSummaryWidget/NutritionSummaryItem.tsx
+++ src/frontend/src/widgets/NutritionSummaryWidget/NutritionSummaryItem.tsx
@@ -11,27 +11,20 @@ export const NutritionSummaryItem: FC<Props> = ({ value, type }) => {
   const { unit, color } = productModel.nutritionValuesConfig[type];
 
   return (
-    <Stack direction="row" alignItems="center">
+    <Stack direction="column" alignItems="center">
       <Box display="flex" justifyContent="center" alignItems="center" p={1}>
         <NutritionValueIcon type={type} size="medium" />
       </Box>
-      <Stack>
-        <Typography
-          variant="body2"
-          component="span"
-          color="textSecondary"
-          textTransform="capitalize"
-        >
-          {type}
+      <Typography variant="body2" component="span" color="textSecondary" textTransform="capitalize">
+        {type}
+      </Typography>
+      <Stack direction="row" spacing={0.5} alignItems="baseline">
+        <Typography variant="subtitle1" component="span" color={color} fontWeight="bold">
+          {value ?? <>&mdash;</>}
         </Typography>
-        <Stack direction="row" spacing={0.5} alignItems="baseline">
-          <Typography variant="subtitle1" component="span" color={color} fontWeight="bold">
-            {value ?? <>&mdash;</>}
-          </Typography>
-          <Typography variant="body2" component="span" color="textSecondary">
-            {unit}
-          </Typography>
-        </Stack>
+        <Typography variant="body2" component="span" color="textSecondary">
+          {unit}
+        </Typography>
       </Stack>
     </Stack>
   );
--- src/frontend/src/widgets/NutritionSummaryWidget/NutritionSummaryWidget.tsx
+++ src/frontend/src/widgets/NutritionSummaryWidget/NutritionSummaryWidget.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Grid2 as Grid } from '@mui/material';
+import { Box, Card, CardContent, CardHeader, Container, Grid2 as Grid } from '@mui/material';
 import { type FC } from 'react';
 import { type productModel } from '@/entities/product';
 import { NutritionSummaryItem } from '@/widgets/NutritionSummaryWidget/NutritionSummaryItem';
@@ -11,27 +11,32 @@ export const NutritionSummaryWidget: FC<Props> = ({ nutritionValues }) => {
   const { calories, protein, fats, carbs, sugar, salt } = nutritionValues;
 
   return (
-    <Box pt={3} pb={2} component={Container}>
-      <Grid container spacing={2}>
-        <Grid size={4} display="flex" justifyContent="center">
-          <NutritionSummaryItem type="calories" value={calories} />
-        </Grid>
-        <Grid size={4} display="flex" justifyContent="center">
-          <NutritionSummaryItem type="protein" value={protein} />
-        </Grid>
-        <Grid size={4} display="flex" justifyContent="center">
-          <NutritionSummaryItem type="fats" value={fats} />
-        </Grid>
-        <Grid size={4} display="flex" justifyContent="center">
-          <NutritionSummaryItem type="carbs" value={carbs} />
-        </Grid>
-        <Grid size={4} display="flex" justifyContent="center">
-          <NutritionSummaryItem type="sugar" value={sugar} />
-        </Grid>
-        <Grid size={4} display="flex" justifyContent="center">
-          <NutritionSummaryItem type="salt" value={salt} />
-        </Grid>
-      </Grid>
+    <Box pt={3} pb={1} component={Container}>
+      <Card>
+        <CardHeader title="Daily Nutrition Summary" titleTypographyProps={{ variant: 'h6' }} />
+        <CardContent>
+          <Grid container spacing={2}>
+            <Grid size={4} display="flex" justifyContent="center">
+              <NutritionSummaryItem type="calories" value={calories} />
+            </Grid>
+            <Grid size={4} display="flex" justifyContent="center">
+              <NutritionSummaryItem type="protein" value={protein} />
+            </Grid>
+            <Grid size={4} display="flex" justifyContent="center">
+              <NutritionSummaryItem type="fats" value={fats} />
+            </Grid>
+            <Grid size={4} display="flex" justifyContent="center">
+              <NutritionSummaryItem type="carbs" value={carbs} />
+            </Grid>
+            <Grid size={4} display="flex" justifyContent="center">
+              <NutritionSummaryItem type="sugar" value={sugar} />
+            </Grid>
+            <Grid size={4} display="flex" justifyContent="center">
+              <NutritionSummaryItem type="salt" value={salt} />
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
     </Box>
   );
 };
