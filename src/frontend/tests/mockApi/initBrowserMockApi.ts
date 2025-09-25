7a678cfb235e11cda9049a0a70626888396fd688
Pavel Kirilin <kirilin.pav@gmail.com>
2025-09-25 22:13:40 +0300

    Add msw registration scope with logging

--- src/frontend/tests/mockApi/initBrowserMockApi.ts
+++ src/frontend/tests/mockApi/initBrowserMockApi.ts
@@ -1,3 +1,5 @@
+import { DEMO_MODE_ENABLED } from '@/shared/config';
+
 const IGNORED_URL_PATTERNS: RegExp[] = [
   /fonts.gstatic.com/,
   /fonts.googleapis.com/,
@@ -13,9 +15,17 @@ export const initBrowserMockApi = async (): Promise<void> => {
 
   await initMockApiDb();
 
+  const basePath = DEMO_MODE_ENABLED ? '/food-diary' : '';
+
+  // eslint-disable-next-line no-console
+  console.info('Starting MSW...');
+
   await worker.start({
     serviceWorker: {
       url: import.meta.env.PROD ? 'serviceWorker.js' : 'mockServiceWorker.js',
+      options: {
+        scope: basePath,
+      },
     },
 
     onUnhandledRequest: (request, print) => {
