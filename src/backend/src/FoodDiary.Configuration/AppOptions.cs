1c8b219a8b5a85b832f8cc61c7afa95c7d8899d6
Pavel Kirilin <kirilin.pav@gmail.com>
2026-08-03 22:13:49 +0300

    refactor: drop Yandex Cloud log format and manual HTTPS scheme option

--- src/backend/src/FoodDiary.Configuration/AppOptions.cs
+++ src/backend/src/FoodDiary.Configuration/AppOptions.cs
@@ -4,13 +4,11 @@ namespace FoodDiary.Configuration;
 
 public class AppOptions
 {
-    public bool ForwardHttpsSchemeManuallyForAllRequests { get; init; }
     public required LoggingOptions Logging { get; init; }
-    
+
     [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
     public class LoggingOptions
     {
         public bool WriteLogsInJsonFormat { get; init; }
-        public bool UseYandexCloudLogsFormat { get; init; }
     }
 }
